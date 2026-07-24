ce10a30ae429131dfeeaf4af405870899962ebe5
Juan Gomez <juan.e.g.f@outlook.com>
2026-07-23 19:45:33 -0600

    chore: consume client in viewmodel and then in apps composable

--- jetCertKMP/shared/src/commonMain/kotlin/okik/tech/jetcert/App.kt
+++ jetCertKMP/shared/src/commonMain/kotlin/okik/tech/jetcert/App.kt
@@ -4,27 +4,36 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import org.jetbrains.compose.resources.painterResource
 
 import jetcert.shared.generated.resources.Res
 import jetcert.shared.generated.resources.compose_multiplatform
+import kotlinx.coroutines.launch
 
 @Composable
 @Preview
 fun App(viewModel: MainViewModel = viewModel { MainViewModel() }) {
     MaterialTheme {
+        val coroutineScope = rememberCoroutineScope()
         val state = viewModel.uiState.collectAsStateWithLifecycle()
 //        var showContent by remember { mutableStateOf(false) }
         Column(
@@ -34,10 +43,14 @@ fun App(viewModel: MainViewModel = viewModel { MainViewModel() }) {
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Button(onClick = { viewModel.toggelShowingContent() }) {
+            Button(onClick = {
+                coroutineScope.launch {
+                    viewModel.getStories()
+                }
+            }) {
                 Text("Click me!")
             }
-            AnimatedVisibility(state.value.shoeContent) {
+            AnimatedVisibility(state.value.showContent) {
 //                val greeting = remember { Greeting().greet() }
 
                 Column(
@@ -45,7 +58,18 @@ fun App(viewModel: MainViewModel = viewModel { MainViewModel() }) {
                     horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
                     Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: ${state.value.greeting}")
+                    LazyColumn {
+                        item {
+                            Text("Top 10 Hacker News Stories", fontSize = 30.sp)
+                            HorizontalDivider()
+                            Spacer(Modifier.height(16.dp))
+                        }
+
+                        items(state.value.stories) { story ->
+                            Text(story.title.orEmpty())
+                            Spacer(Modifier.height(8.dp))
+                        }
+                    }
                 }
             }
         }
--- jetCertKMP/shared/src/commonMain/kotlin/okik/tech/jetcert/MainViewModel.kt
+++ jetCertKMP/shared/src/commonMain/kotlin/okik/tech/jetcert/MainViewModel.kt
@@ -3,17 +3,30 @@ package okik.tech.jetcert
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import okik.tech.jetcert.api.NewsApi
+import okik.tech.jetcert.api.NewsResponse
 
 class MainViewModel : ViewModel() {
 
 //    private val _uiState = MutableStateFlow(UiState(false, Greeting().greet()))
 //    val uiState: StateFlow<UiState> = _uiState.asStateFlow()
     val uiState: StateFlow<UiState>
-            field = MutableStateFlow(UiState(false , Greeting().greet()))
+            field = MutableStateFlow(UiState(false , emptyList()))
 
-    fun toggelShowingContent() = uiState.update { state -> state.copy(shoeContent = !state.shoeContent) }
+    val newsApi: NewsApi = NewsApi()
+
+    fun toggelShowingContent() = uiState.update { state -> state.copy(showContent = !state.showContent) }
+
+    suspend fun getStories() {
+        val topStories = newsApi.getTopStories()
+
+        println("Top stories are ${topStories.map(NewsResponse.Story::title)}")
+
+        uiState.update { uiState ->
+            uiState.copy(showContent = true, topStories)
+        }
+    }
 }
 
-data class UiState(val shoeContent: Boolean, val greeting: String)
+data class UiState(val showContent: Boolean, val stories: List<NewsResponse.Story>)
