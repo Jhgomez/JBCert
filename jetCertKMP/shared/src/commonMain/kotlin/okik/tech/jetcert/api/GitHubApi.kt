6014f0ad1df648f926d8b08b86afa147da5ab5c9
Juan Gomez <juan.e.g.f@outlook.com>
2026-07-25 00:57:53 -0600

    chore: consume key and add loggin interceptor

--- jetCertKMP/shared/src/commonMain/kotlin/okik/tech/jetcert/api/GitHubApi.kt
+++ jetCertKMP/shared/src/commonMain/kotlin/okik/tech/jetcert/api/GitHubApi.kt
@@ -1,17 +1,22 @@
 package okik.tech.jetcert.api
 
 import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.network.http.LoggingInterceptor
 import kotlinx.io.files.Path
+import okik.tech.jetcert.BuildConfig
 
 class GitHubApi {
     // create your token from your github profile /settings/developer Settings
-    private val token: String
-    init {
-        token = ""
-    }
+    private val token: String = BuildConfig.GITHUB_API_KEY
 
     val apolloClient = ApolloClient.Builder()
-        .serverUrl("https://example.com/graphql")
-        .addHttpInterceptor(AuthInterceptor(token))
+        .serverUrl("https://api.github.com/graphql")
+        .httpInterceptors(
+            listOf(
+                AuthInterceptor(token),
+                LoggingInterceptor()
+            )
+        )
+
         .build()
 }
