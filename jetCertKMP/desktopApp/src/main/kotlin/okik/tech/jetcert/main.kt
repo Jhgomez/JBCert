5cafad22ddce5da458e21a41cf044a82369ae37a
Juan Gomez <juan.e.g.f@outlook.com>
2026-07-27 00:03:51 -0600

    chore: init koin call in desktop

--- jetCertKMP/desktopApp/src/main/kotlin/okik/tech/jetcert/main.kt
+++ jetCertKMP/desktopApp/src/main/kotlin/okik/tech/jetcert/main.kt
@@ -2,8 +2,11 @@ package okik.tech.jetcert
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import okik.tech.jetcert.di.initKoinJvm
 
 fun main() = application {
+    initKoinJvm()
+
     Window(
         onCloseRequest = ::exitApplication,
         title = "JetCert",
