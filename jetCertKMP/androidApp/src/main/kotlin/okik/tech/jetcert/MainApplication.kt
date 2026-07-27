5e93931d771d7d975aab819788f9c37b021b59bf
Juan Gomez <juan.e.g.f@outlook.com>
2026-07-26 23:57:34 -0600

    chore: init koin, pass context and register app in maniferst

---
+++ jetCertKMP/androidApp/src/main/kotlin/okik/tech/jetcert/MainApplication.kt
@@ -0,0 +1,17 @@
+package okik.tech.jetcert
+
+import android.app.Application
+import okik.tech.jetcert.di.initKoin
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        initKoin {
+            androidContext(this@MainApplication)
+            androidLogger()
+        }
+    }
+}
