f00b789421e68d7b12af817e928181be962603f3
Juan Gomez <juan.e.g.f@outlook.com>
2026-07-27 00:04:49 -0600

    chore: add android koin module

--- jetCertKMP/shared/src/androidMain/kotlin/okik/tech/jetcert/di/KoinModule.android.kt
+++ jetCertKMP/shared/src/androidMain/kotlin/okik/tech/jetcert/di/KoinModule.android.kt
@@ -1,2 +1,15 @@
-package okik.tech.jetcert.di 
+package okik.tech.jetcert.di
 
+import okik.tech.jetcert.db.AndroidDatabaseDriverFactory
+import okik.tech.jetcert.db.DatabaseDriverFactory
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.module.Module
+import org.koin.dsl.bind
+import org.koin.dsl.module
+import org.koin.plugin.module.dsl.single
+
+actual val platformModule = module {
+    // Classic DSL with lambda for custom construction
+//    single<DatabaseDriverFactory> { AndroidDatabaseDriverFactory(androidContext()) }
+    single<AndroidDatabaseDriverFactory>() bind DatabaseDriverFactory::class
+}
