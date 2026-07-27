fc10210db26c82a2e3e3349ade8913f71a9675df
Juan Gomez <juan.e.g.f@outlook.com>
2026-07-27 00:06:17 -0600

    chore: init koin in web common entry point

--- jetCertKMP/webApp/src/webMain/kotlin/okik/tech/jetcert/main.kt
+++ jetCertKMP/webApp/src/webMain/kotlin/okik/tech/jetcert/main.kt
@@ -2,9 +2,12 @@ package okik.tech.jetcert
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
+import okik.tech.jetcert.di.di.initKoinWeb
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+    initKoinWeb()
+
     ComposeViewport {
         App()
     }
