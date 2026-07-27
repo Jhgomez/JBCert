0b003895bbffff459942746ca83cd5b77700e94e
Juan Gomez <juan.e.g.f@outlook.com>
2026-07-27 00:06:02 -0600

    chore: init koin in jmv entry point

--- jetCertKMP/shared/src/jvmMain/kotlin/okik/tech/jetcert/di/KoinModule.jvm.kt
+++ jetCertKMP/shared/src/jvmMain/kotlin/okik/tech/jetcert/di/KoinModule.jvm.kt
@@ -1,2 +1,14 @@
-package okik.tech.jetcert.di 
+package okik.tech.jetcert.di
 
+import okik.tech.jetcert.db.DatabaseDriverFactory
+import okik.tech.jetcert.db.JvmDatabaseDriverFactory
+import org.koin.core.module.Module
+import org.koin.dsl.bind
+import org.koin.dsl.module
+import org.koin.plugin.module.dsl.single
+
+actual val platformModule: Module = module {
+    // Classic DSL with lambda for custom construction
+//    single { JvmDatabaseDriverFactory() } bind DatabaseDriverFactory::class
+    single<JvmDatabaseDriverFactory>() bind DatabaseDriverFactory::class
+ }
