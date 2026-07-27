547170997ac39dddd78abd25629563ad7e6807dc
Juan Gomez <juan.e.g.f@outlook.com>
2026-07-27 00:04:01 -0600

    chore: init koin call in ios

--- jetCertKMP/iosApp/iosApp/iOSApp.swift
+++ jetCertKMP/iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,12 @@
 import SwiftUI
+import Shared
 
 @main
 struct iOSApp: App {
+    init() {
+        IosKoinInitKt.initializeKoin()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
