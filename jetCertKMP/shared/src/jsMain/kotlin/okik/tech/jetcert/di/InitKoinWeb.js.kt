7721fa216817e811f7c478fdda508da5babcb33b
Juan Gomez <juan.e.g.f@outlook.com>
2026-07-30 09:55:52 -0600

    chore: init koin and inject js driver

---
+++ jetCertKMP/shared/src/jsMain/kotlin/okik/tech/jetcert/di/InitKoinWeb.js.kt
@@ -0,0 +1,26 @@
+package okik.tech.jetcert.di.di
+
+import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.driver.worker.WebWorkerDriver
+import okik.tech.jetcert.db.JetcertDB
+import okik.tech.jetcert.di.initKoin
+import org.koin.core.module.Module
+import org.koin.dsl.module
+import org.w3c.dom.Worker
+
+actual suspend fun koinInit() {
+    val sqlDriver = WebWorkerDriver(
+        Worker(js(""""@cashapp/sqldelight-sqljs-worker/sqljs.worker.js", import.meta.url"""))
+    )
+
+//    JetcertDB.Schema.create(sqlDriver).await()
+
+    val platformModule: Module = module {
+//        single<SqlDriver>(definition = { sqlDriver })
+    }
+
+    initKoin {
+        printLogger()
+        modules(platformModule)
+    }
+}
