3d7c8990c140825d012ebdaf775dd348833882d2
Juan Gomez <juan.e.g.f@outlook.com>
2026-07-30 10:29:58 -0600

    chore: set resource size

--- jetCertKMP/shared/src/commonMain/kotlin/okik/tech/jetcert/App.kt
+++ jetCertKMP/shared/src/commonMain/kotlin/okik/tech/jetcert/App.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.input.TextFieldLineLimits
@@ -110,7 +111,7 @@ fun App() {
                     modifier = Modifier.fillMaxWidth(),
                     horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
+                    Image(painterResource(Res.drawable.compose_multiplatform), null, modifier = Modifier.size(80.dp, 80.dp))
                     LazyColumn {
                         item {
                             Text("Top 10", fontSize = 30.sp)
