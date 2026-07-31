cb4908d7221c39117547c9e261fba1375e98fb70
Juan Gomez <juan.e.g.f@outlook.com>
2026-07-30 20:34:45 -0600

    chore: add notes notes to web entry point

--- jetCertKMP/webApp/src/webMain/kotlin/okik/tech/jetcert/main.kt
+++ jetCertKMP/webApp/src/webMain/kotlin/okik/tech/jetcert/main.kt
@@ -9,6 +9,21 @@ suspend fun main() {
     koinInit()
 
     ComposeViewport {
+    // You could use something like to actually show some loading state if needed
+    // KoinMultiplatformApplication(config = createKoinConfiguration()) {
+    //     var cacheReady by remember { mutableStateOf(false) }
+    //
+    //     LaunchedEffect(Unit) {
+    //         val ksafe: KSafe = getKoin().get()
+    //         ksafe.awaitCacheReady()
+    //         cacheReady = true
+    //     }
+    //
+    //     if (cacheReady) {
+    //         AppContent() // your app's UI (without KoinMultiplatformApplication wrapper)
+    //     }
+    // }
+
         App()
     }
 }
