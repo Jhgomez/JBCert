baa52130f7adb51d25ced92de9476ca7631804fb
Juan Gomez <juan.e.g.f@outlook.com>
2026-07-30 20:34:19 -0600

    chore: add ksafe desktop

--- jetCertKMP/shared/src/jvmMain/kotlin/okik/tech/jetcert/di/KoinInitJvm.kt
+++ jetCertKMP/shared/src/jvmMain/kotlin/okik/tech/jetcert/di/KoinInitJvm.kt
@@ -2,8 +2,10 @@ package okik.tech.jetcert.di
 
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
+import eu.anifantakis.lib.ksafe.KSafe
 import okik.tech.jetcert.db.JetcertDB
 import org.koin.core.module.Module
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 suspend fun initKoinJvm() {
@@ -14,6 +16,15 @@ suspend fun initKoinJvm() {
 
     val jvmModule: Module = module {
         single<SqlDriver> (definition = { driver })
+
+        single(named("prefs")) {
+            KSafe(fileName = "prefs")
+        }
+
+        // Encrypted writes — for secrets (tokens, passwords, PII)
+        single(named("vault")) {
+            KSafe(fileName = "vault")
+        }
     }
 
     initKoin {
