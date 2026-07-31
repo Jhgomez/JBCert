1df93554c304654b7a6b9e6f33ff78048fcf7d97
Juan Gomez <juan.e.g.f@outlook.com>
2026-07-30 20:33:38 -0600

    chore: add ksafe ios

--- jetCertKMP/shared/src/iosMain/kotlin/okik/tech/jetcert/di/InitKoinIos.kt
+++ jetCertKMP/shared/src/iosMain/kotlin/okik/tech/jetcert/di/InitKoinIos.kt
@@ -3,8 +3,10 @@ package okik.tech.jetcert.di
 import app.cash.sqldelight.async.coroutines.synchronous
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.native.NativeSqliteDriver
+import eu.anifantakis.lib.ksafe.KSafe
 import okik.tech.jetcert.db.JetcertDB
 import org.koin.core.module.Module
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 fun init() {
@@ -18,6 +20,14 @@ fun init() {
 //        single { driver } bind SqlDriver::class
 
         single<SqlDriver>(definition = { driver }) // Compiler DSL
+
+        single(named("prefs")) {
+            KSafe(fileName = "prefs")
+        }
+
+        single(named("vault")) {
+            KSafe(fileName = "vault")
+        }
     }
 
     initKoin {
