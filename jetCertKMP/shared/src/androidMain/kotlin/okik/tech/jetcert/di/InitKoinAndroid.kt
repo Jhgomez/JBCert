af11c29bbfd7e73ab4e02c4a1b2c3643b709d841
Juan Gomez <juan.e.g.f@outlook.com>
2026-07-30 20:24:49 -0600

    chore: inject ksafe android

--- jetCertKMP/shared/src/androidMain/kotlin/okik/tech/jetcert/di/InitKoinAndroid.kt
+++ jetCertKMP/shared/src/androidMain/kotlin/okik/tech/jetcert/di/InitKoinAndroid.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import app.cash.sqldelight.async.coroutines.synchronous
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
+import eu.anifantakis.lib.ksafe.KSafe
 import okik.tech.jetcert.db.JetcertDB
+import org.koin.android.ext.koin.androidApplication
+import org.koin.core.qualifier.named
 import org.koin.dsl.KoinAppDeclaration
 import org.koin.dsl.includes
 import org.koin.dsl.module
@@ -22,6 +25,29 @@ fun SetUpKoin(config: KoinAppDeclaration? = null) {
         // single { getDriver(get()) } bind SqlDriver::get       Classic DSL
 
         single { getDriver(get()) } // Classic DSL
+
+        // only use this if your app needs to store not sensitive values, as encryption has a cost
+        // Fast, plain writes — for everyday preferences
+        single(named("prefs")) {
+            KSafe(
+                context = androidApplication(), // get<Context> or just get()
+                fileName = "prefs"
+            )
+        }
+
+        // Encrypted writes — for secrets (tokens, passwords, PII)
+        single(named("vault")) {
+            KSafe(
+                context = get<Context>(),
+                fileName = "vault"
+            )
+        }
+        // you can inject these named koin parameters in different ways, you can do that from the
+        // module that inject the class to koin or for example in a viewmodel like this(note how names matches)
+        // class MyViewModel(
+        //     private val prefs: KSafe,  // @Named("prefs") — fast, unencrypted
+        //     private val vault: KSafe   // @Named("vault") — encrypted secrets
+        // )
     }
 
     initKoin {
