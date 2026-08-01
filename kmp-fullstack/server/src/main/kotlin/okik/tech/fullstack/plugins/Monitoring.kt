c782d2dd43b74b060e8b7b848df535762afcc8ac
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-01 10:10:10 -0600

    chore: add logging/monitoring extension

---
+++ kmp-fullstack/server/src/main/kotlin/okik/tech/fullstack/plugins/Monitoring.kt
@@ -0,0 +1,17 @@
+package okik.tech.fullstack.plugins
+
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.plugins.calllogging.CallLogging
+import io.ktor.server.request.path
+import org.slf4j.event.Level
+
+// used in call logging, this adds structured request logging helpful in debugging and observability
+fun Application.configureMonitoring() {
+    install (CallLogging) {
+        level = Level.INFO
+        filter { call ->
+            call.request.path().startsWith("/api/")
+        }
+    }
+}
--- kmp-fullstack/server/src/main/kotlin/okik/tech/fullstack/plugins/Serialization.kt
+++ kmp-fullstack/server/src/main/kotlin/okik/tech/fullstack/plugins/Serialization.kt
@@ -1,9 +1,11 @@
 package okik.tech.fullstack.plugins
 
-import io.ktor.server.routing.RoutingRoot.Plugin.install
-import javafx.application.Application
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import kotlinx.serialization.json.Json
 
-fun Application.configureSerialization() {
+fun Application.configureSerialization(json: (Any?) -> Unit) {
     // content negotiation allows us to do serialization, the json block is the format we are
     // using in our serialized data, it could be xml or protobuf alternatively if needed
     install(ContentNegotiation) {
