cf8efd160e1be204a913916bd80faba49dffa643
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-02 00:48:16 -0600

    chore: add app config

---
+++ kmp-fullstack/server/src/main/kotlin/okik/tech/fullstack/config/AppConfig.kt
@@ -0,0 +1,13 @@
+package okik.tech.fullstack.config
+
+import org.koin.dsl.module
+
+data class AppConfig(
+    val nasaApiKey: String = System.getenv("NASA_API_KEY") ?: "DEMO_KEY",
+    val cacheDays: Int = System.getenv("CACHE_DAYS")?.toIntOrNull() ?: 90, // how many days we will hold data before cleaning some space
+    val dbFilePath: String = "data/nasa.db"
+)
+
+val appConfigModule = module {
+    single { AppConfig() }
+}
