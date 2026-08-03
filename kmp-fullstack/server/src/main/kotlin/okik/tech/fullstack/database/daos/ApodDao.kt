f8afbfefffb7e5f37164505a739a932c38750c45
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-02 19:55:59 -0600

    bugfix: move object inside entity and move entity to top level class

--- kmp-fullstack/server/src/main/kotlin/okik/tech/fullstack/database/daos/ApodDao.kt
+++ kmp-fullstack/server/src/main/kotlin/okik/tech/fullstack/database/daos/ApodDao.kt
@@ -16,48 +16,15 @@ import java.time.LocalDate
 import kotlin.math.abs
 import kotlin.random.Random
 
-/**
- *  using the DAO pattern https://www.jetbrains.com/help/exposed/get-started-with-exposed-dao.html
- *  however using this new DSL may not be that great since doesn't have the same capability as the
- *  classic DSL, for example it doesn't support "upsert"
- */
 class ApodDao {
-    object Dao : EntityClass<LocalDate, ApodEntity>(Apod)
-
-    class ApodEntity(id: EntityID<LocalDate>) : Entity<LocalDate>(id) {
-
-        var date by Apod.id
-        var copyright by Apod.copyright
-        var fetchedAt by Apod.fetchedAt
-        var explanation by Apod.explanation
-        var url by Apod.url
-        var hdUrl by Apod.hdUrl
-        var media_type by Apod.media_type
-        var title by Apod.title
-        var thumbnailUrl by Apod.thumbnailUrl
-
-
-        inline fun toApodResponse(): ApodResponse =
-            ApodResponse(
-                date = date.toString(), // no formatting as of right now, it will output iso format yyyy-MM-dd by default,
-                title = title,
-                explanation = explanation,
-                url = url,
-                hdUrl = hdUrl,
-                mediaType = media_type,
-                copyright = copyright,
-                thumbnailUrl = thumbnailUrl,
-                fetchedAt = fetchedAt
-            )
-    }
-
-
     suspend fun getByDate(date: LocalDate): ApodResponse? = dbQuery {
-        Dao
-            .findById(date)
-            ?.let(ApodEntity::toApodResponse)
-    }
+        val apod = ApodEntity.Dao.findById(date)
 
+            val result = apod?.let(ApodEntity::toApodResponse)
+
+        result
+
+    }
 
     suspend fun save(apod: ApodResponse): ApodResponse = dbQuery {
         val date = LocalDate.parse(apod.date)
@@ -79,9 +46,9 @@ class ApodDao {
     }
 
     suspend fun getRandom(): ApodResponse? = dbQuery {
-        val randomIndex = Random.nextLong(0, Dao.count() - 1)
+        val randomIndex = Random.nextLong(0, ApodEntity.Dao.count() - 1)
 
-        Dao.all()
+        ApodEntity.Dao.all()
             .offset(randomIndex)
             .limit(1)
             .firstOrNull()
@@ -93,17 +60,17 @@ class ApodDao {
             ((page - 1U) * pageSize).toLong()
         )
 
-        val page = Dao.all()
+        val page = ApodEntity.Dao.all()
             .orderBy(Apod.id to SortOrder.ASC)
             .offset(offset)
             .limit(pageSize.toInt())
             .map(ApodEntity::toApodResponse)
 
-        Pair(page, Dao.count().toUShort())
+        Pair(page, ApodEntity.Dao.count().toUShort())
     }
 
     suspend fun deleteOlderThan(cutoffDate: LocalDate): Int = dbQuery {
-        val entries = Dao.find { Apod.id less cutoffDate }
+        val entries = ApodEntity.Dao.find { Apod.id less cutoffDate }
         val count = entries.count().toInt()
 
         entries.forEach { entry -> entry.delete() }
@@ -113,13 +80,47 @@ class ApodDao {
 
     suspend fun getTotalCount() = 
         dbQuery {
-            Dao.count()
+            ApodEntity.Dao.count()
         }
     
 
     suspend fun countInDateRange(startDate: LocalDate, endDate: LocalDate): Long = dbQuery {
-            Dao
+            ApodEntity.Dao
                 .find { Apod.id greaterEq startDate and (Apod.id lessEq endDate) }
                 .count()
         }
+}
+
+/**
+ *  using the DAO pattern https://www.jetbrains.com/help/exposed/get-started-with-exposed-dao.html
+ *  however using this new DSL may not be that great since doesn't have the same capability as the
+ *  classic DSL, for example it doesn't support "upsert"
+ */
+class ApodEntity(id: EntityID<LocalDate>) : Entity<LocalDate>(id) {
+
+    object Dao : EntityClass<LocalDate, ApodEntity>(Apod)
+
+    var date by Apod.id
+    var copyright by Apod.copyright
+    var fetchedAt by Apod.fetchedAt
+    var explanation by Apod.explanation
+    var url by Apod.url
+    var hdUrl by Apod.hdUrl
+    var media_type by Apod.media_type
+    var title by Apod.title
+    var thumbnailUrl by Apod.thumbnailUrl
+
+
+    inline fun toApodResponse(): ApodResponse =
+        ApodResponse(
+            date = date.toString(), // no formatting as of right now, it will output iso format yyyy-MM-dd by default,
+            title = title,
+            explanation = explanation,
+            url = url,
+            hdUrl = hdUrl,
+            mediaType = media_type,
+            copyright = copyright,
+            thumbnailUrl = thumbnailUrl,
+            fetchedAt = fetchedAt
+        )
 }
--- kmp-fullstack/server/src/main/kotlin/okik/tech/fullstack/database/daos/CacheMetadataDao.kt
+++ kmp-fullstack/server/src/main/kotlin/okik/tech/fullstack/database/daos/CacheMetadataDao.kt
@@ -5,26 +5,25 @@ import org.jetbrains.exposed.v1.core.dao.id.EntityID
 import org.jetbrains.exposed.v1.dao.Entity
 import org.jetbrains.exposed.v1.dao.EntityClass
 
+class CacheMetadataDao {
+    suspend fun set(type: String, currentMillis: String) {
+//        "daily_maintenance_last_run"
+    }
+}
+
 /**
  *  using the DAO pattern https://www.jetbrains.com/help/exposed/get-started-with-exposed-dao.html
  *  however using this new DSL may not be that great since doesn't have the same capability as the
  *  classic DSL, for example it doesn't support "upsert"
  */
-class CacheMetadataDao {
+class CacheMetadataEntity(id: EntityID<String>) : Entity<String>(id) {
     object Dao : EntityClass<String, CacheMetadataEntity>(CacheMetadata)
-
-    class CacheMetadataEntity(id: EntityID<String>) : Entity<String>(id) {
-        var key by CacheMetadata.id
-        var value by CacheMetadata.value
-        var updatedAt by CacheMetadata.updatedAt
+    var key by CacheMetadata.id
+    var value by CacheMetadata.value
+    var updatedAt by CacheMetadata.updatedAt
 
 
-        override fun toString(): String {
-            return "CachedMetadataDao(key=$key, value=$value, updatedAt=$updatedAt)"
-        }
-    }
-
-    suspend fun set(type: String, currentMillis: String) {
-//        "daily_maintenance_last_run"
+    override fun toString(): String {
+        return "CachedMetadataDao(key=$key, value=$value, updatedAt=$updatedAt)"
     }
 }
