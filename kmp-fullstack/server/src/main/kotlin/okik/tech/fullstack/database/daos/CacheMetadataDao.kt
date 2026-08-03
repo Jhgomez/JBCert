f860db8a13fa2620406ce1815e8c2ce786fc3968
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-02 19:00:16 -0600

    refactor: change metadata dao structure

--- kmp-fullstack/server/src/main/kotlin/okik/tech/fullstack/database/daos/CacheMetadataDao.kt
+++ kmp-fullstack/server/src/main/kotlin/okik/tech/fullstack/database/daos/CacheMetadataDao.kt
@@ -10,16 +10,18 @@ import org.jetbrains.exposed.v1.dao.EntityClass
  *  however using this new DSL may not be that great since doesn't have the same capability as the
  *  classic DSL, for example it doesn't support "upsert"
  */
-class CacheMetadataDao(id: EntityID<String>) : Entity<String>(id) {
-    companion object : EntityClass<String, CacheMetadataDao>(CacheMetadata)
+class CacheMetadataDao {
+    object Dao : EntityClass<String, CacheMetadataEntity>(CacheMetadata)
 
-    var key by CacheMetadata.key
-    var value by CacheMetadata.value
-    var updatedAt by CacheMetadata.updatedAt
+    class CacheMetadataEntity(id: EntityID<String>) : Entity<String>(id) {
+        var key by CacheMetadata.key
+        var value by CacheMetadata.value
+        var updatedAt by CacheMetadata.updatedAt
 
 
-    override fun toString(): String {
-        return "CachedMetadataDao(key=$key, value=$value, updatedAt=$updatedAt)"
+        override fun toString(): String {
+            return "CachedMetadataDao(key=$key, value=$value, updatedAt=$updatedAt)"
+        }
     }
 
     suspend fun set(type: String, currentMillis: String) {
