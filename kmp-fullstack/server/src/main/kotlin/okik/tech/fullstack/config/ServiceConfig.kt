3e35ef277a691802d1b2746a97c0cd968fefca1b
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-02 19:58:18 -0600

    chore: add service koin module to inject api client and apod service to di graph

---
+++ kmp-fullstack/server/src/main/kotlin/okik/tech/fullstack/config/ServiceConfig.kt
@@ -0,0 +1,15 @@
+package okik.tech.fullstack.config
+
+import okik.tech.fullstack.services.ApodService
+import okik.tech.fullstack.services.NasaApiClient
+import org.koin.dsl.module
+
+val serviceModule = module {
+    single {
+        val config = get<AppConfig>()
+
+        NasaApiClient(apiKey = config.nasaApiKey)
+    }
+
+    single { ApodService(nasaApiClient = get(), apodDao = get(), cacheMetadataDao = get() ) }
+}
