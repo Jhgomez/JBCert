359e5802786a7f3ce3336616ee1cc43e283ec033
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-04 12:38:41 -0600

    chore: add documentation

--- kmp-fullstack/server/src/main/kotlin/okik/tech/fullstack/plugins/Monitoring.kt
+++ kmp-fullstack/server/src/main/kotlin/okik/tech/fullstack/plugins/Monitoring.kt
@@ -6,7 +6,16 @@ import io.ktor.server.plugins.calllogging.CallLogging
 import io.ktor.server.request.path
 import org.slf4j.event.Level
 
-// used in call logging, this adds structured request logging helpful in debugging and observability
+/**
+ * used in call logging, this adds structured request logging helpful in debugging and observability,
+ * it logs times like "io.ktor.server.Application - 200 OK: GET - /api/apod/today in 88ms" this request
+ * logs can tell us where delays are happening. You could add correlation IDs to each request which will
+ * allow to trace a request across services and even down to specific DB queries. The apod service
+ * logs cache hit rates, it logs whether an register was pulled from NASA's servers or local cache,
+ * which tells us if our caching strategy is working. Simple logging can help start performance optimizations
+ * this means full observability is not, just simple logging done consistently can be helpful to identify
+ * trends, isolate routes and understand cost of different operations
+ */
 fun Application.configureMonitoring() {
     install (CallLogging) {
         level = Level.INFO
--- kmp-fullstack/server/src/main/kotlin/okik/tech/fullstack/plugins/Routing.kt
+++ kmp-fullstack/server/src/main/kotlin/okik/tech/fullstack/plugins/Routing.kt
@@ -4,15 +4,10 @@ import io.ktor.server.application.Application
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
+import okik.tech.fullstack.routes.apodRoutes
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            val javaVersion = System.getProperty("java.version")
-            call.respondText("Ktor: Hello, Java $javaVersion!")
-        }
-        get("/health") {
-            call.respondText("Server is running")
-        }
+        apodRoutes()
     }
 }
