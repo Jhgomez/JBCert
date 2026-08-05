b57b26085cd690ab5d9bbb4ea65ef534ade3d5de
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-04 20:50:57 -0600

    refactor: remove unnecessary dependency

--- kmp-fullstack/server/build.gradle.kts
+++ kmp-fullstack/server/build.gradle.kts
@@ -31,7 +31,6 @@ dependencies {
     implementation(libs.exposed.jdbc)
     implementation(libs.exposed.dao)
     implementation(libs.exposed.java.time)
-    implementation(libs.h2)
     implementation(libs.sqlite.and.jdbc.driver)
     implementation(libs.hikari.connection.pool)
     
