3434786addb207f90845134bedb8717bb37a281f
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-05 17:47:57 -0600

    chore: add apod service

---
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/network/ApodService.kt
@@ -0,0 +1,29 @@
+package okik.tech.fullstack.network
+
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import okik.tech.fullstack.models.ApodResponse
+import okik.tech.fullstack.models.PaginatedResponse
+
+class ApodApiService(
+    private val httpClient: HttpClient,
+    private val baseUrl: String
+) {
+
+    suspend fun getTodaysApod(): ApodResponse {
+        return httpClient.get("$baseUrl/api/apod/today").body()
+    }
+
+    suspend fun getApodHistory(page: Int, pageSize: Int): PaginatedResponse<ApodResponse> {
+        return httpClient.get("$baseUrl/api/apod/history") {
+            parameter("page", page)
+            parameter("pageSize", pageSize)
+        }.body()
+    }
+
+    suspend fun getApodByDate(date: String): ApodResponse {
+        return httpClient.get("$baseUrl/api/apod/date/$date").body()
+    }
+}
