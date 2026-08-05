c64b33e72f15586c895b7a313c9c5f17c856e721
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-05 10:11:47 -0600

    chore: add koin and ktor depedencies to shared module

--- kmp-fullstack/app/shared/build.gradle.kts
+++ kmp-fullstack/app/shared/build.gradle.kts
@@ -67,10 +67,17 @@ kotlin {
             implementation(libs.compose.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+
             implementation(project.dependencies.platform(libs.koin.bom))
             implementation(libs.koin.core)
-//            implementation(libs.koin.cmp)
             implementation(libs.koin.viewmodel)
+//            implementation(libs.koin.cmp)
+
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.cio.engine)
+            implementation(libs.ktor.negotiation.client)
+            implementation(libs.ktor.json)
+            implementation(libs.ktor.logging)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
