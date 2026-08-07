b9b948404c808f67b048f4bccfc4d039f966310f
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-07 17:20:40 -0600

    chore: add init koin fun

--- kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/di/SharedModule.kt
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/di/SharedModule.kt
@@ -5,6 +5,7 @@ import okik.tech.fullstack.data.network.client.NetworkConfig
 import okik.tech.fullstack.data.repository.ApodRepositoryImpl
 import okik.tech.fullstack.domain.ApodRepository
 import okik.tech.fullstack.ui.ApodViewModel
+import org.koin.core.context.startKoin
 import org.koin.dsl.module
 
 val sharedModule = module {
@@ -13,16 +14,18 @@ val sharedModule = module {
     single { NetworkConfig.createHttpClient() }
 
     // API Service
-    single {
-        ApodApiService(
-            httpClient = get(),
-            baseUrl = NetworkConfig.DEFAULT_BASE_URL
-        )
-    }
+    single { ApodApiService(httpClient = get()) }
 
     // Repository
     single<ApodRepository> { ApodRepositoryImpl(get()) }
 
     // ViewModel
     single { ApodViewModel(get()) }
+}
+
+
+fun initKoin() {
+    startKoin {
+        modules(sharedModule)
+    }
 }
