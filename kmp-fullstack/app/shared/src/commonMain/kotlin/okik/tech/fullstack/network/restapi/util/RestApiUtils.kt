1ec42c06c24f64f54abf2f16fb015955fcd82ae6
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-06 22:33:26 -0600

    chore: add safe request, a client-service util

---
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/network/restapi/util/RestApiUtils.kt
@@ -0,0 +1,21 @@
+package okik.tech.fullstack.network.restapi.util
+
+import okik.tech.fullstack.network.client.NotFound
+import okik.tech.fullstack.network.client.Timeout
+import okik.tech.fullstack.network.client.Unauthorized
+import okik.tech.fullstack.network.client.UnknownException
+import okik.tech.fullstack.network.restapi.ApiResult
+
+suspend inline fun <T>safeRequest(block: () -> T): ApiResult<T> {
+    return try {
+        ApiResult.Success(block())
+    } catch (exception: NotFound) {
+        ApiResult.NotFound(exception.reason)
+    } catch (exception: Unauthorized) {
+        ApiResult.Unauthorized(exception.reason)
+    } catch (exception: Timeout) {
+        ApiResult.TimeOut(exception.reason)
+    } catch (exception: UnknownException) {
+        ApiResult.UnknownResult(exception.reason)
+    }
+}
