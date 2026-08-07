d3af0bd81521315c2d50cbf4cdf788ee87fd3046
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-07 15:54:50 -0600

    chore: return proper domain result from repo

--- kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/data/repository/ApodRepositoryImpl.kt
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/data/repository/ApodRepositoryImpl.kt
@@ -1,64 +1,35 @@
-package okik.tech.fullstack.repository
+package okik.tech.fullstack.data.repository
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import okik.tech.fullstack.models.ApodResponse
-import okik.tech.fullstack.models.PaginatedResponse
-import okik.tech.fullstack.network.restapi.ApiResult
-import okik.tech.fullstack.network.restapi.services.ApodApiService
+import okik.tech.fullstack.data.network.restapi.services.ApodApiService
+import okik.tech.fullstack.domain.Apod
+import okik.tech.fullstack.domain.ApodRepository
+import okik.tech.fullstack.domain.DomainResult
+import okik.tech.fullstack.domain.Paging
 
-interface ApodRepository {
-    suspend fun getTodayApod(): Result<ApodResponse>
-    suspend fun getApodHistory(page: Int, pageSize: Int): Result<PaginatedResponse<ApodResponse>>
-    suspend fun getApodByDate(date: String): Result<ApodResponse>
-    fun getTodayApodFlow(): Flow<Result<ApodResponse>>
-}
+class ApodRepositoryImpl(private val apiService: ApodApiService) : ApodRepository {
 
-class ApodRepositoryImpl(
-    private val apiService: ApodApiService
-) : ApodRepository {
+    override suspend fun getTodayApod(): DomainResult<Apod> =
+        apiService
+            .getTodaysApod()
+            .toDomainModel(ApodResponse::toDomainModel)
 
-    override suspend fun getTodayApod(): Result<ApodResponse> {
-        return try {
-            val response = apiService.getTodaysApod()
-            Result.success(response)
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
-    }
-
-    override suspend fun getApodHistory(page: Int, pageSize: Int): Result<PaginatedResponse<ApodResponse>> {
-        try {
-            when(val response = apiService.getApodHistory(page, pageSize)) {
-                is ApiResult.Success<PaginatedResponse<ApodResponse>> -> TODO()
-                is ApiResult.TimeOut<*> -> TODO()
-                is ApiResult.NotFound<*> -> TODO()
-                is ApiResult.Unauthorized<*> -> TODO()
-                is ApiResult.UnknownResult<*> -> TODO()
+    override suspend fun getApodHistory(page: Int, pageSize: Int): DomainResult<Paging<Apod>> =
+        apiService
+            .getApodHistory(page, pageSize)
+            .toDomainModel { dataModel ->
+                dataModel.toDomainModel(ApodResponse::toDomainModel)
             }
-//            Result.success(response)
-        } catch (e: Exception) {
-//            Result.failure(e)
-        }
-    }
 
-    override suspend fun getApodByDate(date: String): Result<ApodResponse> {
-        return try {
-            val response = apiService.getApodByDate(date)
-            Result.success(response)
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
-    }
 
-    override fun getTodayApodFlow(): Flow<Result<ApodResponse>> = flow {
+    override suspend fun getApodByDate(date: String): DomainResult<Apod> =
+        apiService
+            .getApodByDate(date)
+            .toDomainModel(ApodResponse::toDomainModel)
+
+    override fun getTodayApodFlow(): Flow<DomainResult<Apod>> = flow {
         emit(getTodayApod())
     }
 }
-
-// this class should implement a class for whatever codes you expect to receive from your backend
-//sealed interface ApiResult<T> {
-//    data class Success<R>(val data: R): ApiResult<R>
-//    data class NotFound(val message: String): ApiResult<Unit>
-//    data class Unauthorized(val message: String): ApiResult<Unit>
-//}
