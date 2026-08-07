5b9685b6f7262ee5a8ec569d7a703454e00aa4fc
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-07 15:53:34 -0600

    chore: execute http calls inside safe request to return proper api result

--- kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/data/network/restapi/services/ApodService.kt
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/data/network/restapi/services/ApodService.kt
@@ -1,21 +1,21 @@
-package okik.tech.fullstack.network.restapi.services
+package okik.tech.fullstack.data.network.restapi.services
 
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
+import okik.tech.fullstack.data.network.ApiResult
 import okik.tech.fullstack.models.ApodResponse
 import okik.tech.fullstack.models.PaginatedResponse
-import okik.tech.fullstack.network.restapi.ApiResult
-import okik.tech.fullstack.network.restapi.util.safeRequest
+import okik.tech.fullstack.data.network.restapi.util.safeRequest
 
 class ApodApiService(
     private val httpClient: HttpClient,
     private val baseUrl: String
 ) {
 
-    suspend fun getTodaysApod(): ApodResponse {
-        return httpClient.get("$baseUrl/api/apod/today").body()
+    suspend fun getTodaysApod(): ApiResult<ApodResponse> = safeRequest {
+        httpClient.get("$baseUrl/api/apod/today").body()
     }
 
     suspend fun getApodHistory(page: Int, pageSize: Int): ApiResult<PaginatedResponse<ApodResponse>> =
@@ -26,8 +26,7 @@ class ApodApiService(
             }.body()
         }
 
-
-    suspend fun getApodByDate(date: String): ApodResponse {
-        return httpClient.get("$baseUrl/api/apod/date/$date").body()
+    suspend fun getApodByDate(date: String): ApiResult<ApodResponse> = safeRequest {
+        httpClient.get("$baseUrl/api/apod/date/$date").body()
     }
 }
