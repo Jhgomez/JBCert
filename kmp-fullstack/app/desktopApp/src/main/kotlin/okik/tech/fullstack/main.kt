8e5fd4a484b4194d15ef2e153f4d16ff00f109e6
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-07 17:30:43 -0600

    chore: add init koin in web and desktop

--- kmp-fullstack/app/desktopApp/src/main/kotlin/okik/tech/fullstack/main.kt
+++ kmp-fullstack/app/desktopApp/src/main/kotlin/okik/tech/fullstack/main.kt
@@ -2,8 +2,11 @@ package okik.tech.fullstack
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import okik.tech.fullstack.di.initKoin
 
 fun main() = application {
+    initKoin()
+
     Window(
         onCloseRequest = ::exitApplication,
         title = "Fullstack",
--- kmp-fullstack/app/webApp/src/webMain/kotlin/okik/tech/fullstack/main.kt
+++ kmp-fullstack/app/webApp/src/webMain/kotlin/okik/tech/fullstack/main.kt
@@ -2,9 +2,11 @@ package okik.tech.fullstack
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
+import okik.tech.fullstack.di.initKoin
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+    initKoin()
     ComposeViewport {
         App()
     }
