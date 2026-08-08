365fd4d830ec4bf59bff974ba99d11c44002d3ff
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-08 13:58:35 -0600

    chore: add bottom nav bar

--- kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/App.kt
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/App.kt
@@ -9,11 +9,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
 import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -31,7 +34,10 @@ import fullstack.app.shared.generated.resources.Res
 import fullstack.app.shared.generated.resources.compose_multiplatform
 import fullstack.app.shared.generated.resources.gallery
 import fullstack.app.shared.generated.resources.home
+import fullstack.app.shared.generated.resources.info
 import fullstack.app.shared.generated.resources.refresh
+import fullstack.app.shared.generated.resources.search
+import fullstack.app.shared.generated.resources.today
 import okik.tech.fullstack.ui.ApodViewModel
 import org.jetbrains.compose.resources.StringResource
 import org.jetbrains.compose.resources.stringResource
@@ -46,6 +52,23 @@ fun App(viewModel: ApodViewModel = koinViewModel<ApodViewModel>()) {
     }
 }
 
+private var cachedColors: NavigationBarItemColors? = null
+
+val ColorScheme.navigationBatItemColors: NavigationBarItemColors
+    @Composable
+    get() =  cachedColors ?:
+        NavigationBarItemColors(
+            selectedIconColor = Color.White,
+            selectedTextColor = Color.White,
+            selectedIndicatorColor = onPrimaryContainer,
+            unselectedIconColor = Color.LightGray,
+            unselectedTextColor = Color.LightGray,
+            disabledIconColor = Color.DarkGray,
+            disabledTextColor = Color.DarkGray
+        ).also {
+            cachedColors = it
+        }
+
 @Composable
 fun FullStackApp() {
     Scaffold(
@@ -54,9 +77,67 @@ fun FullStackApp() {
         bottomBar = {
             NavigationBar(
                 modifier = Modifier.fillMaxWidth(),
-                containerColor = MaterialTheme.colorScheme.primary
+                containerColor = MaterialTheme.colorScheme.primary,
             ) {
+                NavigationBarItem(
+                    icon = {
+                        Icon(
+                            imageVector = vectorResource(Res.drawable.home),
+                            contentDescription = null
+                        )
+                    },
+                    selected = true,
+                    onClick = {},
+                    label = {
+                        Text("Home")
+                    },
+                    colors = MaterialTheme.colorScheme.navigationBatItemColors
+                )
 
+                NavigationBarItem(
+                    icon = {
+                        Icon(
+                            imageVector = vectorResource(Res.drawable.today),
+                            contentDescription = null
+                        )
+                    },
+                    selected = false,
+                    onClick = {},
+                    label = {
+                        Text("Today")
+                    },
+                    colors = MaterialTheme.colorScheme.navigationBatItemColors
+                )
+
+                NavigationBarItem(
+                    icon = {
+                        Icon(
+                            imageVector = vectorResource(Res.drawable.search),
+                            contentDescription = null
+                        )
+                    },
+                    selected = false,
+                    onClick = {},
+                    label = {
+                        Text("Search")
+                    },
+                    colors = MaterialTheme.colorScheme.navigationBatItemColors
+                )
+
+                NavigationBarItem(
+                    icon = {
+                        Icon(
+                            imageVector = vectorResource(Res.drawable.info),
+                            contentDescription = null
+                        )
+                    },
+                    selected = false,
+                    onClick = {},
+                    label = {
+                        Text("About")
+                    },
+                    colors = MaterialTheme.colorScheme.navigationBatItemColors
+                )
             }
         },
         topBar = {
