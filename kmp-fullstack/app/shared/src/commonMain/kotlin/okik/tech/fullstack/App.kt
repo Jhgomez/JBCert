236a044ba40e6f9f0099fb44941c8f8510dbd845
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-12 22:16:42 -0600

    format: remove unnecessary code

--- kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/App.kt
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/App.kt
@@ -22,7 +22,6 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.adaptive.currentWindowAdaptiveInfoV2
 import androidx.compose.runtime.*
-import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -82,57 +81,6 @@ private enum class TopLevelRoute(
         icon = Res.drawable.info,
         description = Res.string.about
     )
-}
-
-class TopLevelBackStack<T: Any>(startKey: T) {
-
-    // Maintain a stack for each top level route
-    private var topLevelStacks : LinkedHashMap<T, SnapshotStateList<T>> = linkedMapOf(
-        startKey to mutableStateListOf(startKey)
-    )
-
-    // Expose the current top level route for consumers
-    var topLevelKey by mutableStateOf(startKey)
-        private set
-
-    // Expose the back stack so it can be rendered by the NavDisplay
-    val backStack = mutableStateListOf(startKey)
-
-    private fun updateBackStack() =
-        backStack.apply {
-            clear()
-            addAll(topLevelStacks.flatMap { it.value })
-        }
-
-    fun addTopLevel(key: T){
-
-        // If the top level doesn't exist, add it
-        if (topLevelStacks[key] == null){
-            topLevelStacks.put(key, mutableStateListOf(key))
-        } else {
-            // Otherwise just move it to the end of the stacks
-            topLevelStacks.apply {
-                remove(key)?.let {
-                    put(key, it)
-                }
-            }
-        }
-        topLevelKey = key
-        updateBackStack()
-    }
-
-    fun add(key: T){
-        topLevelStacks[topLevelKey]?.add(key)
-        updateBackStack()
-    }
-
-    fun removeLast(){
-        val removedKey = topLevelStacks[topLevelKey]?.removeLastOrNull()
-        // If the removed key was a top level key, remove the associated top level stack
-        topLevelStacks.remove(removedKey)
-        topLevelKey = topLevelStacks.keys.last()
-        updateBackStack()
-    }
 }
 
 @Composable
