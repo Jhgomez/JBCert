471aec159b7be0772b143a6577ead46b60aefda6
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-13 21:24:47 -0600

    bugfix: render issues

--- kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/ui/adaptive/scenedecorators/NavigationDecorator.kt
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/ui/adaptive/scenedecorators/NavigationDecorator.kt
@@ -1,13 +1,24 @@
 package okik.tech.fullstack.ui.adaptive.scenedecorators
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.EnterExitState
 import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.adaptive.WindowAdaptiveInfo
 import androidx.compose.material3.adaptive.currentWindowAdaptiveInfoV2
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.movableContentOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Modifier
 import androidx.navigation3.runtime.NavKey
 import androidx.navigation3.scene.Scene
@@ -15,19 +26,19 @@ import androidx.navigation3.scene.SceneDecoratorStrategy
 import androidx.navigation3.scene.SceneDecoratorStrategyScope
 import androidx.navigation3.ui.LocalNavAnimatedContentScope
 import androidx.window.core.layout.WindowSizeClass
-import androidx.window.core.layout.WindowSizeClass.Companion.WIDTH_DP_LARGE_LOWER_BOUND
-import androidx.window.core.layout.WindowSizeClass.Companion.WIDTH_DP_MEDIUM_LOWER_BOUND
+import okik.tech.fullstack.ui.adaptive.cacheSize
 
 class NavigationDecoratorScene(
-    scene: Scene<NavKey>,
+    private val scene: Scene<NavKey>,
     private val windowSizeClass: WindowSizeClass,
     private val sharedTransitionScope: SharedTransitionScope,
-    private val enableBackNavigation: Boolean,
+    private val enableBackNavigation: MutableState<Boolean>,
     private val navBar: @Composable () -> Unit,
     private val navRail: @Composable () -> Unit,
     private val topBar:  @Composable () -> Unit,
     private val navIcon:  @Composable () -> Unit
 ) : Scene<NavKey> by scene {
+    override val key = scene::class to scene.key
 
     override val content = @Composable {
         val animatedContentScope = LocalNavAnimatedContentScope.current
@@ -35,44 +46,48 @@ class NavigationDecoratorScene(
             animatedContentScope.transition.targetState == EnterExitState.Visible
 
         with(sharedTransitionScope) {
-            when {
-                windowSizeClass.isWidthAtLeastBreakpoint(WIDTH_DP_MEDIUM_LOWER_BOUND) -> {
-                    scene.content()
+            if (windowSizeClass.isWidthAtLeastBreakpoint(WindowSizeClass.WIDTH_DP_MEDIUM_LOWER_BOUND)) {
+                Row(Modifier.fillMaxSize()) {
+//                    Box(
+//                        modifier = Modifier
+////                            .cacheSize(!isMovableContentCaller)
+//                            .sharedElement(
+//                                rememberSharedContentState("nav-rail"),
+//                                animatedContentScope
+//                            )
+//                    ) {
+//                        if (isMovableContentCaller) {
+//                            navRail()
+//                        }
+//                    }
+                    Box(modifier = Modifier.weight(1f)) {
+                        scene.content()
+                    }
                 }
+            } else {
+                Column(Modifier.fillMaxSize()) {
+                    AnimatedVisibility(
+                        visible = enableBackNavigation.value,
+                        enter = fadeIn() + expandVertically(),
+                        exit = shrinkVertically() + fadeOut()
+                    ) {
+                        topBar()
+                    }
 
-                windowSizeClass.isWidthAtLeastBreakpoint(WIDTH_DP_LARGE_LOWER_BOUND) -> {
-                    scene.content()
-                }
+                    Box(modifier = Modifier.weight(1f)) {
+                        scene.content()
+                    }
 
-                else -> {
-                    Column (Modifier.fillMaxSize()) {
-                        Box(
-                            modifier = Modifier
-                                .sharedElement(
-                                    rememberSharedContentState("top-bar"),
-                                    animatedContentScope
-                                )
-                        ) {
-                            if (
-//                                isMovableContentCaller &&
-                                enableBackNavigation) {
-                                topBar()
-                            }
-                        }
-                        Box(modifier = Modifier.weight(1f)) {
-                            scene.content()
-                        }
-                        Box(
-                            modifier = Modifier
-                                .sharedElement(
-                                    rememberSharedContentState("nav-rail"),
-                                    animatedContentScope
-                                )
-                        ) {
-//                            if (isMovableContentCaller) {
-//                                navBar()
-//                            }
-                            topBar()
+                    Box(
+                        modifier = Modifier
+                            .cacheSize(!isMovableContentCaller)
+                            .sharedElement(
+                                rememberSharedContentState("nav-bar"),
+                                animatedContentScope
+                            )
+                    ) {
+                        if (isMovableContentCaller) {
+                            navBar()
                         }
                     }
                 }
@@ -84,20 +99,26 @@ class NavigationDecoratorScene(
 @Composable
 fun rememberDecoratorStrategy(
     sharedTransitionScope: SharedTransitionScope,
-    enableBackNavigation: Boolean,
+    enableBackNavigation: MutableState<Boolean>,
     navBar: @Composable () -> Unit,
     navRail: @Composable () -> Unit,
     topBar:  @Composable () -> Unit,
-    navIcon:  @Composable () -> Unit
+    navIcon:  @Composable () -> Unit,
+    windowSizeClass: WindowSizeClass = currentWindowAdaptiveInfoV2().windowSizeClass,
 ): DecoratorStrategy {
-   val sizeClass = currentWindowAdaptiveInfoV2().windowSizeClass
 
-    return remember(sizeClass) {
+    val currentNavBar by rememberUpdatedState(navBar)
+    val currentNavRail by rememberUpdatedState(navRail)
+
+    val movableNavBar = remember { movableContentOf { currentNavBar() } }
+    val movableNavRail = remember { movableContentOf { currentNavRail() } }
+
+    return remember(windowSizeClass, sharedTransitionScope) {
         DecoratorStrategy(
-            windowSizeClass = sizeClass,
+            windowSizeClass = windowSizeClass,
             sharedTransitionScope = sharedTransitionScope,
-            navBar = navBar,
-            navRail = navRail,
+            navBar = movableNavBar,
+            navRail = movableNavRail,
             enableBackNavigation = enableBackNavigation,
             topBar = topBar,
             navIcon = navIcon,
@@ -108,7 +129,7 @@ fun rememberDecoratorStrategy(
 class DecoratorStrategy(
     private val windowSizeClass: WindowSizeClass,
     private val sharedTransitionScope: SharedTransitionScope,
-    private val enableBackNavigation: Boolean,
+    private val enableBackNavigation: MutableState<Boolean>,
     private val navBar: @Composable (() -> Unit),
     private val navRail: @Composable (() -> Unit),
     private val topBar:  @Composable () -> Unit,
