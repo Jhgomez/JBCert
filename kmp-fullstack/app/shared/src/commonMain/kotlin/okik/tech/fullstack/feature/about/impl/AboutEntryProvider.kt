4ea8f64694b8d60d23113a48c0be21dfeba247f9
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-14 20:49:48 -0600

    refactor: extract about screen

--- kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/feature/about/impl/AboutEntryProvider.kt
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/feature/about/impl/AboutEntryProvider.kt
@@ -1,23 +1,11 @@
 package okik.tech.fullstack.feature.about.impl
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.navigation3.runtime.EntryProviderScope
 import androidx.navigation3.runtime.NavKey
 import okik.tech.fullstack.feature.about.api.AboutHome
 
 fun EntryProviderScope<NavKey>.aboutEntry() {
     entry<AboutHome> {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text("About")
-        }
+        AboutScreen()
     }
 }
