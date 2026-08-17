3c27475759e062271b2d7968fc34bc5814a3b45d
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-16 23:41:28 -0600

    chore: add home list vm

---
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/feature/home/impl/ApodList/HomeLIstViewModel.kt
@@ -0,0 +1,10 @@
+package okik.tech.fullstack.feature.home.impl.ApodList
+
+import androidx.lifecycle.ViewModel
+import okik.tech.fullstack.domain.ApodRepository
+
+class HomeLIstViewModel(
+    private val apodRepository: ApodRepository
+): ViewModel() {
+
+}
