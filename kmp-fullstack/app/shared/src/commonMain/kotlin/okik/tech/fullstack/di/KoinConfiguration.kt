c04e518c6be25c83a20f9809d7a214b6db2cd23e
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-22 17:06:59 -0600

    chore: enable pass configuration to pass android context

--- kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/di/KoinConfiguration.kt
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/di/KoinConfiguration.kt
@@ -3,9 +3,12 @@ package okik.tech.fullstack.di
 import okik.tech.fullstack.data.network.di.networkModule
 import okik.tech.fullstack.data.repository.di.repositoryModule
 import org.koin.core.context.startKoin
+import org.koin.dsl.KoinAppDeclaration
+import org.koin.dsl.includes
 
-fun initKoin() {
+fun initKoin(config: KoinAppDeclaration? = null) {
     startKoin {
+        includes(config)
         modules(
             networkModule,
             repositoryModule
