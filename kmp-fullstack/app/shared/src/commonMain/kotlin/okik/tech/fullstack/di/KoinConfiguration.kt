001db379fef3676fd26d7c68f28a1963c39b2c6e
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-23 17:31:37 -0600

    chore: add feature koin module in main koin config

--- kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/di/KoinConfiguration.kt
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/di/KoinConfiguration.kt
@@ -3,6 +3,7 @@ package okik.tech.fullstack.di
 import okik.tech.fullstack.data.di.dataModule
 import okik.tech.fullstack.data.network.di.networkModule
 import okik.tech.fullstack.data.repository.di.repositoryModule
+import okik.tech.fullstack.feature.di.featureModule
 import org.koin.core.context.startKoin
 import org.koin.dsl.KoinAppDeclaration
 import org.koin.dsl.includes
@@ -11,7 +12,8 @@ fun initKoin(config: KoinAppDeclaration? = null) {
     startKoin {
         includes(config)
         modules(
-            dataModule
+            dataModule,
+            featureModule
         )
     }
 }
