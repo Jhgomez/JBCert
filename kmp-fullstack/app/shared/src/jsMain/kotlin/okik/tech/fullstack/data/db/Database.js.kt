faa47c8f9bfe8dc1b6f63b73bf9921d07a9b1ecf
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-22 20:27:49 -0600

    chore: add js sql driver producer

---
+++ kmp-fullstack/app/shared/src/jsMain/kotlin/okik/tech/fullstack/data/db/Database.js.kt
@@ -0,0 +1,23 @@
+package okik.tech.fullstack.data.db
+
+import app.cash.sqldelight.async.coroutines.awaitCreate
+import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.driver.worker.WebWorkerDriver
+import okik.tech.fullstack.db.FullstackDb
+import org.w3c.dom.Worker
+
+class IosDbDriverProducerImpl(): DbDriverFactory {
+    override suspend fun asynchronousGet(): SqlDriver {
+        val sqlDriver = WebWorkerDriver(
+            Worker(
+                js(
+                    """new URL("@cashapp/sqldelight-sqljs-worker/sqljs.worker.js", import.meta.url)"""
+                )
+            )
+        )
+
+        FullstackDb.Schema.awaitCreate(sqlDriver)
+
+        return sqlDriver
+    }
+}
