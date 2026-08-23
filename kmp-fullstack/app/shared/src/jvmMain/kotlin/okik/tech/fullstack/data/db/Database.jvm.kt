3bfaa526de70f9f00f742b6414a829daaf811552
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-22 20:28:09 -0600

    chore: add jvm sql driver producer

---
+++ kmp-fullstack/app/shared/src/jvmMain/kotlin/okik/tech/fullstack/data/db/Database.jvm.kt
@@ -0,0 +1,17 @@
+package okik.tech.fullstack.data.db
+
+import app.cash.sqldelight.async.coroutines.synchronous
+import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
+import okik.tech.fullstack.db.FullstackDb
+import java.util.Properties
+
+
+class IosDbDriverProducerImpl(): DbDriverFactory {
+    override fun synchronousGet(): SqlDriver =
+        JdbcSqliteDriver(
+            url = "jdbc:sqlite:/path/to/myDatabase.db ", //JdbcSqliteDriver.IN_MEMORY,
+            properties = Properties(),
+            schema = FullstackDb.Schema.synchronous()
+        )
+}
