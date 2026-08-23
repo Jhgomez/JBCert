ff013565c3882b81aab49489d8168e6998efb6b4
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-22 21:09:22 -0600

    refactor: remove unncessar boiler plate factor

--- kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/data/db/Database.kt
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/data/db/Database.kt
@@ -3,14 +3,6 @@ package okik.tech.fullstack.data.db
 import app.cash.sqldelight.db.SqlDriver
 import okik.tech.fullstack.db.FullstackDb
 
-interface DbDriverFactory {
-    // This is used in all targets but web
-    fun synchronousGet(): SqlDriver? = null
-
-    // This is used in Web targets
-    suspend fun asynchronousGet(): SqlDriver? = null
-}
-
 fun createDatabase(sqlDriver: SqlDriver) : FullstackDb {
     return FullstackDb(sqlDriver)
 }
