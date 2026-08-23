ae1c62e26014bb06ac75b9c9f8951bc5880f6778
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-22 21:15:40 -0600

    refactor: delete jvm boiler plate driver factory and create it directly in koin module

--- kmp-fullstack/app/shared/src/jvmMain/kotlin/okik/tech/fullstack/data/db/di/DatabaseModule.jvm.kt
+++ kmp-fullstack/app/shared/src/jvmMain/kotlin/okik/tech/fullstack/data/db/di/DatabaseModule.jvm.kt
@@ -1,10 +1,19 @@
 package okik.tech.fullstack.data.db.di
 
+import app.cash.sqldelight.async.coroutines.synchronous
 import app.cash.sqldelight.db.SqlDriver
-import okik.tech.fullstack.data.db.JvmDbDriverProducerImpl
+import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
+import okik.tech.fullstack.db.FullstackDb
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import java.util.Properties
 
 actual val driverModule: Module = module {
-    single<SqlDriver> { JvmDbDriverProducerImpl().synchronousGet() }
+    single<SqlDriver> {
+        JdbcSqliteDriver(
+            url = "jdbc:sqlite:/path/to/myDatabase.db ", //JdbcSqliteDriver.IN_MEMORY,
+            properties = Properties(),
+            schema = FullstackDb.Schema.synchronous()
+        )
+    }
 }
