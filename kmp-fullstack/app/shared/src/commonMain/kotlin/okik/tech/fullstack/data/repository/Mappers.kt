23ede00b9157d54e2689d6f83ff654a4013a45e8
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-23 11:24:40 -0600

    refactor: add mappers from new data error result hierarchy to domain error result and from entity to domain model

--- kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/data/repository/Mappers.kt
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/data/repository/Mappers.kt
@@ -1,16 +1,19 @@
 package okik.tech.fullstack.data.repository
 
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.format.byUnicodePattern
 import okik.tech.fullstack.data.network.ApiResult
+import okik.tech.fullstack.db.ApodEntity
 import okik.tech.fullstack.domain.Apod
 import okik.tech.fullstack.domain.DomainError
 import okik.tech.fullstack.domain.DomainResult
 
 import okik.tech.fullstack.domain.Paging
 import okik.tech.fullstack.models.ApodResponse
-import okik.tech.fullstack.models.ErrorResponse
 import okik.tech.fullstack.models.PaginatedResponse
+import kotlin.time.Instant
 
-fun <T, R> PaginatedResponse<T>.toDomainModel(mapper: (T) -> R) = Paging(
+fun <T, R> PaginatedResponse<T>.toDomainErrorModel(mapper: (T) -> R) = Paging(
     items = items.map(mapper),
     page = page,
     pageSize = pageSize,
@@ -18,28 +21,49 @@ fun <T, R> PaginatedResponse<T>.toDomainModel(mapper: (T) -> R) = Paging(
     totalPages = totalPages
 )
 
-fun ApodResponse.toDomainModel() = Apod(
-    date = date,
+fun ApodEntity.toDomainModel() = Apod(
+    date = LocalDate.fromEpochDays(dateId).toString(),
     title = title,
     explanation = explanation,
     url = url,
     hdUrl = hdUrl,
-    mediaType = mediaType,
+    mediaType = media_type,
     copyright = copyright,
     thumbnailUrl = thumbnailUrl,
-    fetchedAt = fetchedAt
+    fetchedAt = Instant.fromEpochMilliseconds(fetchedAt).toString()
 )
 
-fun ErrorResponse.toDomainModel() = DomainError(
-    status = status,
-    message = message
-)
-
-fun <T, R> ApiResult<T>.toDomainModel(successMapper: (dataModel: T) -> R): DomainResult<R> = when(this) {
-    is ApiResult.NotFound<T> -> DomainResult.DomainErrorResult.NotFound(errorResponse.toDomainModel())
-    is ApiResult.Success<T> -> DomainResult.Success(successMapper.invoke(this.result))
-    is ApiResult.NetworkError<T> -> DomainResult.DomainErrorResult.NetworkError(errorResponse.toDomainModel())
-    is ApiResult.Unauthorized<T> -> DomainResult.DomainErrorResult.Unauthorized(errorResponse.toDomainModel())
-    is ApiResult.UnknownResult<T> -> DomainResult.DomainErrorResult.UnknownResult(errorResponse.toDomainModel())
-    is ApiResult.UnhandledHttpCode<*> -> DomainResult.DomainErrorResult.UnhandledHttpCode(errorResponse.toDomainModel())
+val dateFormat = LocalDate.Format {
+    byUnicodePattern("yyyy-MM-dd")
 }
+
+fun ApodResponse.toApodEntity() = ApodEntity(
+    dateId = dateFormat.parse(date).toEpochDays(),
+    copyright = copyright,
+    fetchedAt = fetchedAt,
+    explanation = explanation,
+    url = url,
+    hdUrl = hdUrl,
+    media_type = mediaType,
+    title = title,
+    thumbnailUrl = thumbnailUrl
+)
+
+fun <T: Any> ApiResult.Error<T>.toDomainError() = DomainError(
+    status = errorResponse.status,
+    message = errorResponse.message
+)
+
+fun <T : Any, R> ApiResult.Error<T>.toDomainResultError(): DomainResult.DomainErrorResult<R> =
+    when(this) {
+        is ApiResult.Error.NotFound<T> -> 
+            DomainResult.DomainErrorResult.NotFound(toDomainError())
+        is ApiResult.Error.NetworkError<T> -> 
+            DomainResult.DomainErrorResult.NetworkError(toDomainError())
+        is ApiResult.Error.Unauthorized<T> ->
+            DomainResult.DomainErrorResult.Unauthorized(toDomainError())
+        is ApiResult.Error.UnknownResult<T> -> 
+            DomainResult.DomainErrorResult.UnknownResult(toDomainError())
+        is ApiResult.Error.UnhandledHttpCode<*> -> 
+            DomainResult.DomainErrorResult.UnhandledHttpCode(toDomainError())
+    }
