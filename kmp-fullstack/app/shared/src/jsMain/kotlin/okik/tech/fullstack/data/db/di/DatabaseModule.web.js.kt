5540665d315d62ea81ffb7977cf2125bdcc13e4f
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-22 21:14:42 -0600

    refactor: delete js boiler plate driver factory and create it directly in koin module

--- kmp-fullstack/app/shared/src/jsMain/kotlin/okik/tech/fullstack/data/db/di/DatabaseModule.web.js.kt
+++ kmp-fullstack/app/shared/src/jsMain/kotlin/okik/tech/fullstack/data/db/di/DatabaseModule.web.js.kt
@@ -1,14 +1,25 @@
 package okik.tech.fullstack.data.db.di
 
+import app.cash.sqldelight.async.coroutines.awaitCreate
 import app.cash.sqldelight.db.SqlDriver
-import okik.tech.fullstack.data.db.JsDbDriverProducerImpl
+import app.cash.sqldelight.driver.worker.WebWorkerDriver
+import okik.tech.fullstack.db.FullstackDb
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import org.w3c.dom.Worker
 
 actual suspend fun getDriverModule(): Module {
-    val driver = JsDbDriverProducerImpl().asynchronousGet()
+    val sqlDriver = WebWorkerDriver(
+        Worker(
+            js(
+                """new URL("@cashapp/sqldelight-sqljs-worker/sqljs.worker.js", import.meta.url)"""
+            )
+        )
+    )
+
+    FullstackDb.Schema.awaitCreate(sqlDriver)
 
     return module {
-        single<SqlDriver> { driver }
+        single<SqlDriver> { sqlDriver }
     }
 }
