8408d829e364069db6da686b0c03af229846547f
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-23 17:32:32 -0600

    chore: declare koin feature module

--- kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/feature/di/FeatureModule.kt
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/feature/di/FeatureModule.kt
@@ -1,2 +1,11 @@
 package okik.tech.fullstack.feature.di
 
+import okik.tech.fullstack.feature.home.impl.ApodList.HomeListViewModel
+import org.koin.core.module.dsl.viewModel
+import org.koin.dsl.module
+
+val featureModule = module {
+    viewModel {
+        HomeListViewModel(apodRepository = get())
+    }
+}
