2878ce97a9f21deed0b6fd3c47aedd849430a15d
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-22 21:12:36 -0600

    refactor: delete ios boiler plate driver factory and create it directly in koin module

--- kmp-fullstack/app/shared/src/iosMain/kotlin/okik/tech/fullstack/data/db/di/DatabaseModule.ios.kt
+++ kmp-fullstack/app/shared/src/iosMain/kotlin/okik/tech/fullstack/data/db/di/DatabaseModule.ios.kt
@@ -1,11 +1,17 @@
 package okik.tech.fullstack.data.db.di
 
+import app.cash.sqldelight.async.coroutines.synchronous
 import app.cash.sqldelight.db.SqlDriver
-import okik.tech.fullstack.data.db.DbDriverFactory
-import okik.tech.fullstack.data.db.IosDbDriverFactoryImpl
+import app.cash.sqldelight.driver.native.NativeSqliteDriver
+import okik.tech.fullstack.db.FullstackDb
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 actual val driverModule: Module = module {
-    single<SqlDriver> { IosDbDriverFactoryImpl().synchronousGet() }
+    single<SqlDriver> {
+        NativeSqliteDriver(
+            schema = FullstackDb.Schema.synchronous(),
+            name = "fullstackDb"
+        )
+    }
 }
