ec2a53ffff67188b6490719fd9747827e24897c5
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-22 21:19:27 -0600

    chore: call custom web init koin

--- kmp-fullstack/app/webApp/src/webMain/kotlin/okik/tech/fullstack/main.kt
+++ kmp-fullstack/app/webApp/src/webMain/kotlin/okik/tech/fullstack/main.kt
@@ -2,11 +2,13 @@ package okik.tech.fullstack
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
+import okik.tech.fullstack.di.WebMainInitKoin
 import okik.tech.fullstack.di.initKoin
 
 @OptIn(ExperimentalComposeUiApi::class)
-fun main() {
-    initKoin()
+suspend fun main() {
+    WebMainInitKoin()
+
     ComposeViewport {
         App()
     }
