1d260e100dce0b1783dcbb3de4637a00f943124e
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-22 20:36:29 -0600

    refactor: rename impl

--- kmp-fullstack/app/shared/src/androidMain/kotlin/okik/tech/fullstack/data/db/Database.android.kt
+++ kmp-fullstack/app/shared/src/androidMain/kotlin/okik/tech/fullstack/data/db/Database.android.kt
@@ -7,7 +7,7 @@ import app.cash.sqldelight.driver.android.AndroidSqliteDriver
 import okik.tech.fullstack.db.FullstackDb
 
 
-class AndroidDbDriverProducerImpl(private val context: Context): DbDriverFactory {
+class AndroidDbDriverFactoryImpl(private val context: Context): DbDriverFactory {
     override fun synchronousGet(): SqlDriver =
         AndroidSqliteDriver(
             schema = FullstackDb.Schema.synchronous(),
--- kmp-fullstack/app/shared/src/androidMain/kotlin/okik/tech/fullstack/data/db/di/DatabaseModule.android.kt
+++ kmp-fullstack/app/shared/src/androidMain/kotlin/okik/tech/fullstack/data/db/di/DatabaseModule.android.kt
@@ -1,11 +1,11 @@
 package okik.tech.fullstack.data.db.di
 
-import okik.tech.fullstack.data.db.AndroidDbDriverProducerImpl
+import okik.tech.fullstack.data.db.AndroidDbDriverFactoryImpl
 import okik.tech.fullstack.data.db.DbDriverFactory
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 actual val driverModule: Module = module {
-    single<DbDriverFactory> { AndroidDbDriverProducerImpl(context = androidContext()) }
+    single<DbDriverFactory> { AndroidDbDriverFactoryImpl(context = androidContext()) }
 }
