baaa3dc9168358aaa2628d15feae5bfb15c8a66b
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-22 21:17:28 -0600

    refactor: delete wasm boiler plate driver factory and create it directly in koin module

--- kmp-fullstack/app/shared/src/wasmJsMain/kotlin/okik/tech/fullstack/data/db/di/DatabaseModule.web.wasmJs.kt
+++ kmp-fullstack/app/shared/src/wasmJsMain/kotlin/okik/tech/fullstack/data/db/di/DatabaseModule.web.wasmJs.kt
@@ -1,13 +1,22 @@
 package okik.tech.fullstack.data.db.di
 
+import app.cash.sqldelight.async.coroutines.awaitCreate
 import app.cash.sqldelight.db.SqlDriver
-import okik.tech.fullstack.data.db.DbDriverFactory
-import okik.tech.fullstack.data.db.WasmDbDriverProducerImpl
+import app.cash.sqldelight.driver.worker.WebWorkerDriver
+import okik.tech.fullstack.db.FullstackDb
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import org.w3c.dom.Worker
+
+@OptIn(ExperimentalWasmJsInterop::class)
+val worker: Worker = js(
+    """new Worker(new URL("@cashapp/sqldelight-sqljs-worker/sqljs.worker.js", import.meta.url))"""
+)
 
 actual suspend fun getDriverModule(): Module {
-    val driver = WasmDbDriverProducerImpl().asynchronousGet()
+    val driver = WebWorkerDriver(worker)
+
+    FullstackDb.Schema.awaitCreate(driver)
 
     return module {
         single<SqlDriver> { driver }
