122620b1f7a44aee2e371662e701a522e0278ad3
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-23 23:59:18 -0600

    chore: encapsulate apod mappers

---
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/data/repository/mappers/Apod.kt
@@ -0,0 +1,36 @@
+package okik.tech.fullstack.data.repository.mappers
+
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.format.byUnicodePattern
+import okik.tech.fullstack.db.ApodEntity
+import okik.tech.fullstack.domain.Apod
+import okik.tech.fullstack.models.ApodResponse
+import kotlin.time.Instant
+
+fun ApodEntity.toDomainModel() = Apod(
+    date = LocalDate.fromEpochDays(dateId).toString(),
+    title = title,
+    explanation = explanation,
+    url = url,
+    hdUrl = hdUrl,
+    mediaType = media_type,
+    copyright = copyright,
+    thumbnailUrl = thumbnailUrl,
+    fetchedAt = Instant.fromEpochMilliseconds(fetchedAt).toString()
+)
+
+val dateFormat = LocalDate.Format {
+    byUnicodePattern("yyyy-MM-dd")
+}
+
+fun ApodResponse.toApodEntity() = ApodEntity(
+    dateId = dateFormat.parse(date).toEpochDays(),
+    copyright = copyright,
+    fetchedAt = fetchedAt,
+    explanation = explanation,
+    url = url,
+    hdUrl = hdUrl,
+    media_type = mediaType,
+    title = title,
+    thumbnailUrl = thumbnailUrl
+)
