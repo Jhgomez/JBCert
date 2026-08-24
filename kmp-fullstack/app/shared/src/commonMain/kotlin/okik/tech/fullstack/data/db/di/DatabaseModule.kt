654998b4a996334d75f2de03c414e05cff52944b
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-23 22:13:31 -0600

    chore: create value class and inject it from teargets driver module

--- kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/data/db/di/DatabaseModule.kt
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/data/db/di/DatabaseModule.kt
@@ -1,15 +1,21 @@
 package okik.tech.fullstack.data.db.di
 
+import kotlinx.coroutines.CoroutineDispatcher
 import okik.tech.fullstack.data.db.ApodDao
 import okik.tech.fullstack.data.db.createDatabase
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 expect val driverModule: Module
+value class DaoDispatcher(val dispatcher: CoroutineDispatcher)
 
 val databaseModule = module {
     includes(driverModule)
 
     single { createDatabase(sqlDriver = get()) }
-    single { ApodDao(database = get()) }
+
+    single {
+        val daoDispatcher: DaoDispatcher = get()
+        ApodDao(database = get(), dispatcher = daoDispatcher.dispatcher)
+    }
 }
