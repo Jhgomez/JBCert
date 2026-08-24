448ac858ec94a9c82399a9104749b18b11cbde35
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-24 16:50:52 -0600

    chore: enable sqldelight logger

--- kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/data/db/Database.kt
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/data/db/Database.kt
@@ -1,8 +1,16 @@
 package okik.tech.fullstack.data.db
 
 import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.logs.LogSqliteDriver
+import okik.tech.fullstack.Logger
 import okik.tech.fullstack.db.FullstackDb
 
 fun createDatabase(sqlDriver: SqlDriver) : FullstackDb {
-    return FullstackDb(sqlDriver)
+
+    val driverLoggerWrapper = LogSqliteDriver(
+        sqlDriver = sqlDriver,
+        logger = { message -> Logger.logInfo("SqlDelightLogging", message) }
+    )
+
+    return FullstackDb(driverLoggerWrapper)
 }
