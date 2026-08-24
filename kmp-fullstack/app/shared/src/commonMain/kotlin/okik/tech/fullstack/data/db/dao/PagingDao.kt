3f7da1b003f582b08a3af0658cd8ebaf5f87f071
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-23 23:37:49 -0600

    chore: implement paging dao int

--- kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/data/db/dao/PagingDao.kt
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/data/db/dao/PagingDao.kt
@@ -1,23 +1,23 @@
 package okik.tech.fullstack.data.db.dao
 
-import app.cash.sqldelight.async.coroutines.awaitAsList
 import app.cash.sqldelight.async.coroutines.awaitAsOne
-import app.cash.sqldelight.paging3.QueryPagingSource
-import kotlinx.coroutines.CoroutineDispatcher
-import okik.tech.fullstack.db.ApodEntity
 import okik.tech.fullstack.db.FullstackDb
 import okik.tech.fullstack.db.PageInfoEntity
 
-class PagingDao(val database: FullstackDb, val dispatcher: CoroutineDispatcher) {
-    val queries = database.pagingQueries
+class PagingInfoDaoImpl(
+    val database: FullstackDb
+): PagingInfoDao<PageInfoEntity> {
+    private val queries = database.pagingQueries
 
-    suspend fun selectAll(pageId: String): PageInfoEntity = queries.selectPageInfo(pageId).awaitAsOne()
+    override suspend fun select(id: String): PageInfoEntity = queries.selectPageInfo(id).awaitAsOne()
 
-    suspend fun upsertPage(page: PageInfoEntity): PageInfoEntity {
-        queries.upsert(page)
+    override suspend fun upsertPage(entry: PageInfoEntity): PageInfoEntity {
+        queries.upsert(entry)
 
-        return page
+        return entry
     }
 
-    suspend fun delete() = queries.delete()
+    override suspend fun delete() {
+        queries.delete()
+    }
 }
