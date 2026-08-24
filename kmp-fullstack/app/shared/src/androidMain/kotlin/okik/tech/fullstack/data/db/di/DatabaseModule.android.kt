b57fd486b0f35ab18b8499dd32b407b1a4412cd5
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-23 22:15:31 -0600

    chore: inject dispatcher from android

--- kmp-fullstack/app/shared/src/androidMain/kotlin/okik/tech/fullstack/data/db/di/DatabaseModule.android.kt
+++ kmp-fullstack/app/shared/src/androidMain/kotlin/okik/tech/fullstack/data/db/di/DatabaseModule.android.kt
@@ -3,12 +3,15 @@ package okik.tech.fullstack.data.db.di
 import app.cash.sqldelight.async.coroutines.synchronous
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
+import kotlinx.coroutines.Dispatchers
 import okik.tech.fullstack.db.FullstackDb
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 actual val driverModule: Module = module {
+    single<DaoDispatcher> { DaoDispatcher(Dispatchers.IO) }
+
     single<SqlDriver> {
         AndroidSqliteDriver(
             schema = FullstackDb.Schema.synchronous(),
@@ -16,4 +19,6 @@ actual val driverModule: Module = module {
             name = "fullstackDb"
         )
     }
+
+
 }
