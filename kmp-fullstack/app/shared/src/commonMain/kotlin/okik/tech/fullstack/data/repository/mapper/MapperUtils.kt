3d86a92b8a56cdffb833edb1c112bfa5b41d54f6
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-24 13:51:50 -0600

    refactor: use new domain error signature in mapper

--- kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/data/repository/mapper/MapperUtils.kt
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/data/repository/mapper/MapperUtils.kt
@@ -1,24 +1,18 @@
 package okik.tech.fullstack.data.repository.mapper
 
 import okik.tech.fullstack.data.network.ApiResult
-import okik.tech.fullstack.domain.DomainError
 import okik.tech.fullstack.domain.DomainResult
 
-fun <T: Any> ApiResult.Error<T>.toDomainError() = DomainError(
-    status = errorResponse.status,
-    message = errorResponse.message
-)
-
-fun <T : Any, R> ApiResult.Error<T>.toDomainResultError(): DomainResult.DomainErrorResult<R> =
+fun <T : Any> ApiResult.Error<T>.toDomainResultError(): DomainResult.DomainErrorResult =
     when(this) {
         is ApiResult.Error.NotFound<T> -> 
-            DomainResult.DomainErrorResult.NotFound(toDomainError())
+            DomainResult.DomainErrorResult.NotFound(errorResponse.status, errorResponse.message)
         is ApiResult.Error.NetworkError<T> -> 
-            DomainResult.DomainErrorResult.NetworkError(toDomainError())
+            DomainResult.DomainErrorResult.NetworkError(errorResponse.status, errorResponse.message)
         is ApiResult.Error.Unauthorized<T> ->
-            DomainResult.DomainErrorResult.Unauthorized(toDomainError())
+            DomainResult.DomainErrorResult.Unauthorized(errorResponse.status, errorResponse.message)
         is ApiResult.Error.UnknownResult<T> -> 
-            DomainResult.DomainErrorResult.UnknownResult(toDomainError())
+            DomainResult.DomainErrorResult.UnknownResult(errorResponse.status, errorResponse.message)
         is ApiResult.Error.UnhandledHttpCode<*> -> 
-            DomainResult.DomainErrorResult.UnhandledHttpCode(toDomainError())
+            DomainResult.DomainErrorResult.UnhandledHttpCode(errorResponse.status, errorResponse.message)
     }
