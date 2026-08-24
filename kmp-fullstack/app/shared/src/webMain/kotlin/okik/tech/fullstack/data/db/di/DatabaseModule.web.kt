cd420432de5927dc1ed319504de41141d4ebab47
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-23 22:16:17 -0600

    chore: inject dao dispatcher from web

--- kmp-fullstack/app/shared/src/webMain/kotlin/okik/tech/fullstack/data/db/di/DatabaseModule.web.kt
+++ kmp-fullstack/app/shared/src/webMain/kotlin/okik/tech/fullstack/data/db/di/DatabaseModule.web.kt
@@ -1,10 +1,13 @@
 package okik.tech.fullstack.data.db.di
 
+import kotlinx.coroutines.Dispatchers
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 // empty module since web targets can not create a sql driver synchronously, it has to be
 // asynchronously and we should not force other targets to create it synchronously
-actual val driverModule: Module = module { }
+actual val driverModule: Module = module {
+    single { DaoDispatcher(Dispatchers.Default) }
+}
 
 expect suspend fun getDriverModule(): Module
