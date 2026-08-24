1f762f4d1cb1079db78115d6686702acff58eb5b
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-24 03:55:58 -0600

    chore: add pagI source factory

---
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/data/db/paging/PagingSourceFactory.kt
@@ -0,0 +1,17 @@
+package okik.tech.fullstack.data.db.paging
+
+import app.cash.sqldelight.paging3.QueryPagingSource
+import kotlinx.coroutines.CoroutineDispatcher
+import okik.tech.fullstack.db.FullstackDb
+
+class PagingSourceFactory(database: FullstackDb, private val dispatcher: CoroutineDispatcher) {
+    val queries = database.apodQueries
+
+    fun pagingSource() = QueryPagingSource(
+        countQuery = queries.countApods(),
+        transacter = queries,
+        context = dispatcher,
+        queryProvider = queries::apodsPage
+    )
+
+}
