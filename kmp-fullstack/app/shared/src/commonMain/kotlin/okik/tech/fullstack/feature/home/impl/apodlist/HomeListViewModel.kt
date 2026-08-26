fa66757dda0830b60bb23e435f68badca4c8fccb
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-25 22:49:21 -0600

    chore: add home list view model

--- kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/feature/home/impl/apodlist/HomeListViewModel.kt
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/feature/home/impl/apodlist/HomeListViewModel.kt
@@ -1,10 +1,25 @@
-package okik.tech.fullstack.feature.home.impl.ApodList
+package okik.tech.fullstack.feature.home.impl.apodlist
 
 import androidx.lifecycle.ViewModel
+import androidx.paging.PagingData
+import kotlinx.coroutines.flow.Flow
+import okik.tech.fullstack.domain.Apod
 import okik.tech.fullstack.domain.ApodRepository
 
-class HomeLIstViewModel(
-    private val apodRepository: ApodRepository
-): ViewModel() {
+/**
+ * This is a very simple screen there is no need to create a UiState class, tbe state is all
+ * provided/driven by the PagingData Flow, the PagingState success is always converted into a flow
+ * of type domain model APOD, and errors from network are translated into error of domain layer that
+ * are then passed, again, by Paging data, the errors from the domain model extend from exception
+ * class so that we can wrap them around MediatorResult.Error class, and all of this has to be handled
+ */
+class HomeListViewModel(private val apodRepository: ApodRepository): ViewModel() {
 
+    // this is the paying price(unchecked cast) for following CA while leveraging androidx paging3 pager
+    val apodPagesFlow: Flow<PagingData<Apod>> =
+        apodRepository.getApodPagingFlow() as Flow<PagingData<Apod>>
+
+    fun refreshPagerData() {
+//        apodRepository.refreshPagerData()
+    }
 }
