5a4f3ad0119cc16988ed8fd918acfa7a4db8f51a
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-25 22:43:33 -0600

    chore: add params to nav keys

--- kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/feature/home/api/HomeNavKeys.kt
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/feature/home/api/HomeNavKeys.kt
@@ -1,6 +1,7 @@
 package okik.tech.fullstack.feature.home.api
 
 import androidx.navigation3.runtime.NavKey
+import coil3.memory.MemoryCache
 import kotlinx.serialization.Serializable
 import okik.tech.fullstack.domain.Apod
 
@@ -11,4 +12,8 @@ sealed interface Home: NavKey
 object HomeList: Home
 
 @Serializable
-class HomeApodDetail(val apod: Apod?): Home
+class HomeApodDetail(
+    val apod: Apod,
+    val coilCacheKey: String?,
+    val keyExtras: Map<String, String>?
+): Home
