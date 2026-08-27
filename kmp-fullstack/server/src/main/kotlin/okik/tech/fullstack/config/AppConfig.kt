922b17760aa00a6c5d5f541f7d9f2fdfe638bebd
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-26 19:08:26 -0600

    chore: add cache directories info

--- kmp-fullstack/server/src/main/kotlin/okik/tech/fullstack/config/AppConfig.kt
+++ kmp-fullstack/server/src/main/kotlin/okik/tech/fullstack/config/AppConfig.kt
@@ -5,7 +5,9 @@ import org.koin.dsl.module
 data class AppConfig(
     val nasaApiKey: String = System.getenv("NASA_API_KEY") ?: "DEMO_KEY",
     val cacheDays: Int = System.getenv("CACHE_DAYS")?.toIntOrNull() ?: 90, // how many days we will hold data before cleaning some space
-    val dbFilePath: String = "generated-db/apod.db"
+    val dbFilePath: String = "generated-db/apod.db",
+    val cacheDirectory: String = "~/JBCert/kmp-fullstack/cache-files",
+    val cacheTempDirectory: String = "~/JBCert/kmp-fullstack/cache-temp"
 )
 
 val appConfigModule = module {
