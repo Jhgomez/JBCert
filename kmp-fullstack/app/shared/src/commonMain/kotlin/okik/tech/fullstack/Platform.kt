41a897e44f0d5f0b09a23d94f20d2425922730c2
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-27 10:44:29 -0600

    refactor: allow public access to value class property

--- kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/Platform.kt
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/Platform.kt
@@ -3,7 +3,7 @@ package okik.tech.fullstack
 import kotlin.jvm.JvmInline
 
 @JvmInline
-value class Platform(private val baseUrl: String) {
+value class Platform(val baseUrl: String) {
     fun getHostName(): String = baseUrl.substring(0, baseUrl.indexOf(":"))
 
     fun getPort(): UShort = baseUrl.substring(baseUrl.indexOf(":") + 1).toUShort()
