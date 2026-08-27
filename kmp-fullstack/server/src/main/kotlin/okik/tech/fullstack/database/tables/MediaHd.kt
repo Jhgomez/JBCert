e605f366ba700ee81667f202f3b85c518e7ac531
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-26 20:01:49 -0600

    choreL add media server exposed sqlite db table

---
+++ kmp-fullstack/server/src/main/kotlin/okik/tech/fullstack/database/tables/MediaHd.kt
@@ -0,0 +1,14 @@
+package okik.tech.fullstack.database.tables
+
+import org.jetbrains.exposed.v1.core.Column
+import org.jetbrains.exposed.v1.core.dao.id.EntityID
+import org.jetbrains.exposed.v1.core.dao.id.IdTable
+
+object MediaHd : IdTable<String>("media_hd") {
+    override val id: Column<EntityID<String>> = varchar("hdurl", 256).entityId()
+    override val primaryKey = PrimaryKey(id)
+
+    val pictureFile = varchar("file", 256)
+    val contentType = varchar("content_type", 64)
+}
+
