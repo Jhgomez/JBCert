5df093e9a04fa7ffb0e612e9513cebd908a81f9b
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-27 02:09:44 -0600

    refactor: change to absolute path

--- kmp-fullstack/server/src/main/kotlin/okik/tech/fullstack/config/AppConfig.kt
+++ kmp-fullstack/server/src/main/kotlin/okik/tech/fullstack/config/AppConfig.kt
@@ -6,9 +6,9 @@ data class AppConfig(
     val nasaApiKey: String = System.getenv("NASA_API_KEY") ?: "DEMO_KEY",
     val cacheDays: Int = System.getenv("CACHE_DAYS")?.toIntOrNull() ?: 90, // how many days we will hold data before cleaning some space
     val dbFilePath: String = "generated-db/apod.db",
-    val cacheDirectory: String = "~/JBCert/kmp-fullstack/cachefiles",
-    val cacheHdDirectory: String = "~/JBCert/kmp-fullstack/cachehdfiles",
-    val cacheTempDirectory: String = "~/JBCert/kmp-fullstack/cachetemp"
+    val cacheDirectory: String = "C:/Users/Urtit/JBCert/kmp-fullstack/cachefiles",
+    val cacheHdDirectory: String = "C:/Users/Urtit/JBCert/kmp-fullstack/cachehdfiles",
+    val cacheTempDirectory: String = "C:/Users/Urtit/JBCert/kmp-fullstack/cachetemp"
 )
 
 val appConfigModule = module {
