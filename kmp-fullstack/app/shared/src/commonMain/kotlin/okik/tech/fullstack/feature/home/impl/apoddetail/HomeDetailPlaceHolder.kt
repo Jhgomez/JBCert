c589637034e51e678d5951bc15291c964e18662b
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-27 12:17:07 -0600

    chore: add home detail placeholder

---
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/feature/home/impl/apoddetail/HomeDetailPlaceHolder.kt
@@ -0,0 +1,29 @@
+package okik.tech.fullstack.feature.home.impl.apoddetail
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import fullstack.app.shared.generated.resources.Res
+import fullstack.app.shared.generated.resources.apod_list_placeholder_message
+import fullstack.app.shared.generated.resources.gallery
+import org.jetbrains.compose.resources.stringResource
+
+@Composable
+fun HomeDetailPlaceholder(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .clip(MaterialTheme.shapes.medium)
+            .background(MaterialTheme.colorScheme.secondaryFixedDim),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = stringResource(Res.string.apod_list_placeholder_message),
+            style = MaterialTheme.typography.titleLargeEmphasized
+        )
+    }
+}
