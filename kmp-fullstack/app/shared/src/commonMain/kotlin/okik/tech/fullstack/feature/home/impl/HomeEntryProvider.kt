539b23277dd61c401d590d7704cd2487656d6ad0
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-28 21:27:35 -0600

    chore: register detail nav entry composable

--- kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/feature/home/impl/HomeEntryProvider.kt
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/feature/home/impl/HomeEntryProvider.kt
@@ -1,14 +1,9 @@
 package okik.tech.fullstack.feature.home.impl
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
 import androidx.compose.material3.adaptive.navigation3.ListDetailSceneStrategy
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation3.runtime.EntryProviderScope
@@ -16,6 +11,7 @@ import androidx.navigation3.runtime.NavKey
 import okik.tech.fullstack.feature.home.api.HomeApodDetail
 import okik.tech.fullstack.feature.home.api.HomeList
 import okik.tech.fullstack.feature.home.impl.apoddetail.ApodDetailPlaceholder
+import okik.tech.fullstack.feature.home.impl.apoddetail.ApodDetailScreen
 import okik.tech.fullstack.feature.home.impl.apodlist.HomeListScreen
 import okik.tech.fullstack.navigation.exitthroughhome.ExitThroughHomeNavigator
 
@@ -46,28 +42,6 @@ fun EntryProviderScope<NavKey>.homeEntries(
                 )
             }
         )
-
-
-//        Column(
-//            modifier = Modifier.fillMaxSize(),
-//            horizontalAlignment = Alignment.CenterHorizontally,
-//            verticalArrangement = Arrangement.Center
-//        ) {
-//            var placeholder: MemoryCache.Key? = remember { null }
-////            var bitmap: MutableState<Painter> = remember { mutableStateOf(ColorPainter(Color.LightGray)) }
-//
-////            LaunchedEffect(navigator) {
-////                bitmap.value = BitmapPainter(Res.readBytes("drawable/placeholder.webp").decodeToImageBitmap())
-////            }
-//
-//
-//
-//            Button(onClick = {
-//                navigator.navigate(HomeApodDetail(null))
-//            }) {
-//                Text("To Apod")
-//            }
-//        }
     }
 
     entry<HomeApodDetail>(
@@ -75,16 +49,11 @@ fun EntryProviderScope<NavKey>.homeEntries(
             sceneKey = HomeList
         )
     ) { key ->
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Button(onClick = {
-                navigator.goBack()
-            }) {
-                Text(key.apod.toString())
-            }
-        }
+        ApodDetailScreen(
+            apod = key.apod,
+            coilCacheKey = key.coilCacheKey,
+            keyExtras = key.keyExtras,
+            modifier = Modifier.fillMaxSize()
+        )
     }
 }
