a6131879026c949e8768b5c03693931c972875f3
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-28 19:33:20 -0600

    chore: extend today nav keys by new app nav key interface

--- kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/feature/today/api/TodayNavKeys.kt
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/feature/today/api/TodayNavKeys.kt
@@ -3,12 +3,15 @@ package okik.tech.fullstack.feature.today.api
 import androidx.navigation3.runtime.NavKey
 import kotlinx.serialization.Serializable
 import okik.tech.fullstack.domain.Apod
+import okik.tech.fullstack.navigation.AppNavKey
 
 @Serializable
-sealed interface Today: NavKey
+sealed interface Today: AppNavKey
 
 @Serializable
 object TodayHome: Today
 
 @Serializable
-class TodayDetail(val apod: Apod?): Today
+class TodayDetail(val apod: Apod?): Today {
+    override val shouldNavIcon: Boolean = true
+}
