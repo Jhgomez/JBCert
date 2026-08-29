0836d272cc5e843895f94df5ba5195b32fedf2bb
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-28 19:30:14 -0600

    chore: extend about nav keys by new app nav key interface

--- kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/feature/about/api/AboutNavKey.kt
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/feature/about/api/AboutNavKey.kt
@@ -2,9 +2,11 @@ package okik.tech.fullstack.feature.about.api
 
 import androidx.navigation3.runtime.NavKey
 import kotlinx.serialization.Serializable
+import okik.tech.fullstack.navigation.AppNavKey
 
 @Serializable
-sealed interface About: NavKey
+sealed interface About: AppNavKey
+
 
 @Serializable
 object AboutHome: About
