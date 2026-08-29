58a0f11cd5e9440226fdf4668ecf16990a9a3fe1
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-28 19:32:10 -0600

    chore: extend search nav keys by new app nav key interface

--- kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/feature/search/api/SearchNavKeys.kt
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/feature/search/api/SearchNavKeys.kt
@@ -3,12 +3,15 @@ package okik.tech.fullstack.feature.search.api
 import androidx.navigation3.runtime.NavKey
 import kotlinx.serialization.Serializable
 import okik.tech.fullstack.domain.Apod
+import okik.tech.fullstack.navigation.AppNavKey
 
 @Serializable
-sealed interface Search: NavKey
+sealed interface Search: AppNavKey
 
 @Serializable
 object SearchHome: Search
 
 @Serializable
-class SearchDetail(val apod: Apod?): Search
+class SearchDetail(val apod: Apod?): Search {
+    override val shouldShowTopBar: Boolean = true
+}
