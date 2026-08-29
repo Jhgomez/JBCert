fc123411c4a297ff7b951a7005b36bbb5f4eb2d8
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-28 19:32:52 -0600

    chore: extend home nav keys by new app nav key interface

--- kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/feature/home/api/HomeNavKeys.kt
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/feature/home/api/HomeNavKeys.kt
@@ -4,9 +4,10 @@ import androidx.navigation3.runtime.NavKey
 import coil3.memory.MemoryCache
 import kotlinx.serialization.Serializable
 import okik.tech.fullstack.domain.Apod
+import okik.tech.fullstack.navigation.AppNavKey
 
 @Serializable
-sealed interface Home: NavKey
+sealed interface Home: AppNavKey
 
 @Serializable
 object HomeList: Home
@@ -16,4 +17,6 @@ class HomeApodDetail(
     val apod: Apod,
     val coilCacheKey: String?,
     val keyExtras: Map<String, String>?
-): Home
+): Home {
+    override val shouldNavIcon: Boolean = true
+}
