5f94985b33330945cb79715aaa19f8cb063e5383
Juan Gomez <juan.e.g.f@outlook.com>
2026-08-31 23:21:15 -0600

    chore: register higher koin module in lower feature koin module

--- kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/feature/di/FeatureModule.kt
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/feature/di/FeatureModule.kt
@@ -1,10 +1,12 @@
 package okik.tech.fullstack.feature.di
 
 import okik.tech.fullstack.feature.home.impl.di.homeFeatureModule
+import okik.tech.fullstack.feature.today.impl.di.todayFeatureModule
 import org.koin.dsl.module
 
 val featureModule = module {
     includes(
-        homeFeatureModule
+        homeFeatureModule,
+        todayFeatureModule
     )
 }
