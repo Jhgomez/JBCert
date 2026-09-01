085efb1626b29c9dff28b58821ae56b094a11933
Juan Gomez <juan.e.g.f@outlook.com>
2026-09-01 15:18:23 -0600

    refactor: keep date as epoch day with long type

--- kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/domain/Repositories.kt
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/domain/Repositories.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.flow.Flow
 interface ApodRepository {
     suspend fun getTodayApod(): DomainResult<Apod>
     suspend fun getApodHistory(page: Int, pageSize: Int): DomainResult<Paging<Apod>>
-    suspend fun getApodByDate(date: String): DomainResult<Apod>
+    suspend fun getApodByDate(date: Long): DomainResult<Apod>
     fun getTodayApodFlow(): Flow<DomainResult<Apod>>
     suspend fun upsertPageInfo(pageInfo: PageInfo): PageInfo
     suspend fun deletePagesInfo()
