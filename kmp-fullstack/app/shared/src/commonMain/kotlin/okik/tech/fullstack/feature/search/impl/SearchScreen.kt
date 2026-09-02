48e537727bb755f80720e580875107f75347cb98
Juan Gomez <juan.e.g.f@outlook.com>
2026-09-01 19:14:06 -0600

    chore: add padding and shape to fab

--- kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/feature/search/impl/SearchScreen.kt
+++ kmp-fullstack/app/shared/src/commonMain/kotlin/okik/tech/fullstack/feature/search/impl/SearchScreen.kt
@@ -5,13 +5,14 @@ import androidx.compose.animation.SharedTransitionLayout
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.LargeFloatingActionButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumFloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBarDefaults
@@ -24,11 +25,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import coil3.memory.MemoryCache
 import fullstack.app.shared.generated.resources.Res
 import fullstack.app.shared.generated.resources.calendar
 import kotlinx.datetime.LocalDate
-import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.atStartOfDayIn
 import kotlinx.datetime.toLocalDateTime
@@ -106,10 +107,10 @@ fun SearchScreen(
                     scrollBehavior = scrollBehavior
                 )
 
-                LargeFloatingActionButton(
-                    modifier = Modifier.align(Alignment.BottomEnd),
+                MediumFloatingActionButton(
+                    modifier = Modifier.align(Alignment.BottomEnd).padding(16.dp),
                     onClick = { showModal.value = true },
-                    shape = CircleShape,
+                    shape = MaterialTheme.shapes.medium,
                 ) {
                     Icon(
                         imageVector = vectorResource(Res.drawable.calendar),
