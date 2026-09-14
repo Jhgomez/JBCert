c936b0054f8cca558ac40348dfa9211d07175416
Juan Gomez <juan.e.g.f@outlook.com>
2026-09-14 13:21:22 -0600

    chore: initiate dagger compoment

--- DaggerExample/app/src/main/java/okik/tech/daggerexample/DaggerDemoApp.kt
+++ DaggerExample/app/src/main/java/okik/tech/daggerexample/DaggerDemoApp.kt
@@ -1,7 +1,9 @@
 package okik.tech.daggerexample
 
 import android.app.Application
+import okik.tech.daggerexample.di.AppComponent
+import okik.tech.daggerexample.di.DaggerAppComponent
 
 class DaggerDemoApp : Application() {
-
+    val appComponent: AppComponent = DaggerAppComponent.create()
 }
