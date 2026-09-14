87881118da4125581c34a4469a69d3a98985b105
Juan Gomez <juan.e.g.f@outlook.com>
2026-09-14 13:25:44 -0600

    chore: inject main activity

--- DaggerExample/app/src/main/java/okik/tech/daggerexample/MainActivity.kt
+++ DaggerExample/app/src/main/java/okik/tech/daggerexample/MainActivity.kt
@@ -17,6 +17,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        (application as DaggerDemoApp).getAppComponent().inject(this)
+
         setContent {
             DaggerExampleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
