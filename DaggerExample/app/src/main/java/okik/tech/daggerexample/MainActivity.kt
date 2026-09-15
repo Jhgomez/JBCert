e95415a35304dda623496bf62b853a82695d1a02
Juan Gomez <juan.e.g.f@outlook.com>
2026-09-14 21:00:25 -0600

    chore: inject factory and retrive viewmodel

--- DaggerExample/app/src/main/java/okik/tech/daggerexample/MainActivity.kt
+++ DaggerExample/app/src/main/java/okik/tech/daggerexample/MainActivity.kt
@@ -1,9 +1,11 @@
 package okik.tech.daggerexample
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,40 +13,32 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModelProvider
 import okik.tech.daggerexample.ui.theme.DaggerExampleTheme
+import javax.inject.Inject
 
 class MainActivity : ComponentActivity() {
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+    private val viewModel by  viewModels <DemoVm> { viewModelFactory }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         (application as DaggerDemoApp).getAppComponent().inject(this)
 
+        viewModel.executeRepoA()
+
         setContent {
             DaggerExampleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    Text(
+                        text = viewModel.screenAState.value.repoAResult,
+                        Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DaggerExampleTheme {
-        Greeting("Android")
     }
 }
